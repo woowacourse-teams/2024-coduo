9dfe5caaf9f011842082701ac2f7f4e48946f51d
kelly6bf <kelly6bf@gmail.com>
2024-07-15 15:52:43 +0900

    chore: #13 Init Backend Project

---
+++ backend/src/main/java/site/coduo/coduo/CoduoApplication.java
@@ -0,0 +1,13 @@
+package site.coduo.coduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoduoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/site/coduo/coduo/CoduoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.coduo.coduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
