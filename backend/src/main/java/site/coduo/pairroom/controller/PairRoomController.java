fd84c679e104a95f3e0d575b8475e4b8bbe6d863
reddevilmidzy <midzy0228@gmail.com>
2024-07-17 19:24:13 +0900

    style: 사용하지 않는 import 문 제거

--- backend/src/main/java/site/coduo/pairroom/controller/PairRoomController.java
+++ backend/src/main/java/site/coduo/pairroom/controller/PairRoomController.java
@@ -2,21 +2,19 @@ package site.coduo.pairroom.controller;
 
 import java.net.URI;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import site.coduo.pairroom.controller.docs.PairRoomDocs;
 import site.coduo.pairroom.dto.PairRoomCreateRequest;
-import site.coduo.pairroom.dto.PairRoomReadRequest;
 import site.coduo.pairroom.dto.PairRoomCreateResponse;
+import site.coduo.pairroom.dto.PairRoomReadRequest;
 import site.coduo.pairroom.dto.PairRoomReadResponse;
 import site.coduo.pairroom.service.PairRoomService;
 
@@ -28,13 +26,16 @@ public class PairRoomController implements PairRoomDocs {
     private final PairRoomService service;
 
     @GetMapping("/pair-room")
-    public ResponseEntity<PairRoomReadResponse> getPairRoom(@RequestParam("accessCode") final PairRoomReadRequest accessCode) {
-        final PairRoomReadResponse response = PairRoomReadResponse.from(service.findByAccessCode(accessCode.accessCode()));
+    public ResponseEntity<PairRoomReadResponse> getPairRoom(
+            @RequestParam("accessCode") final PairRoomReadRequest accessCode) {
+        final PairRoomReadResponse response = PairRoomReadResponse.from(
+                service.findByAccessCode(accessCode.accessCode()));
         return ResponseEntity.ok(response);
     }
 
     @PostMapping("/pair-room")
-    public ResponseEntity<PairRoomCreateResponse> createPairRoom(@RequestBody final PairRoomCreateRequest pairRoomCreateRequest) {
+    public ResponseEntity<PairRoomCreateResponse> createPairRoom(
+            @RequestBody final PairRoomCreateRequest pairRoomCreateRequest) {
         final PairRoomCreateResponse response = new PairRoomCreateResponse(service.save(pairRoomCreateRequest));
         return ResponseEntity.created(URI.create("/"))
                 .body(response);
--- backend/src/main/java/site/coduo/pairroom/domain/AccessCode.java
+++ backend/src/main/java/site/coduo/pairroom/domain/AccessCode.java
@@ -5,9 +5,7 @@ import java.util.UUID;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
