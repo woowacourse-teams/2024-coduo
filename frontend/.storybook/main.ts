4da6313dc27ebe94a65a02739b0dcbe094b03b6e
greetings1012 <ahnqwerty@gmail.com>
2024-07-19 16:34:08 +0900

    💄 Header 스토리북 구현

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)', '../src/**/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
@@ -9,6 +10,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-viewport',
   ],
   framework: {
     name: '@storybook/react-webpack5',
@@ -23,5 +25,11 @@ const config: StorybookConfig = {
       },
     },
   }),
+  webpackFinal: async (config) => {
+    if (config.resolve) {
+      config.resolve.plugins = [...(config.resolve.plugins || []), new TsconfigPathsPlugin()];
+    }
+    return config;
+  },
 };
 export default config;
