a461fee0aae44a147174d3974c8535b338d0385d
greetings1012 <ahnqwerty@gmail.com>
2024-07-19 16:29:47 +0900

    🍱 에셋 파일 관리를 위한 index.ts 추가

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,5 @@
+import logo_icon from '@/assets/images/logo_icon.svg';
+import logo_icon_with_title from '@/assets/images/logo_icon_with_title.svg';
+import logo_title from '@/assets/images/logo_title.svg';
+
+export { logo_icon, logo_icon_with_title, logo_title };
