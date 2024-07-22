5057c20d2c35f53c6fa635a9cb8386b02884c354
greetings1012 <ahnqwerty@gmail.com>
2024-07-22 14:58:52 +0900

    ⏪️ App에 잘못 올라간 header 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { ThemeProvider } from 'styled-components';
 
-import Header from '@/components/common/Header/Header';
-
 import GlobalStyles from './styles/Global.style';
 import { theme } from './styles/theme';
 
@@ -9,9 +7,7 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div>
-        <Header />
-      </div>
+      <div></div>
     </ThemeProvider>
   );
 };
