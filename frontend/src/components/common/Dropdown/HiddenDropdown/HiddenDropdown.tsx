6e035b0d944e10fe7fb77abe7f499dc11c8bb1b9
greetings1012 <ahnqwerty@gmail.com>
2024-07-22 21:00:17 +0900

    ♿️ 웹 접근성 향상을 위한 HiddenDropdown 구현

---
+++ frontend/src/components/common/Dropdown/HiddenDropdown/HiddenDropdown.tsx
@@ -0,0 +1,27 @@
+import { HTMLAttributes } from 'react';
+
+interface HiddenDropdownProps extends HTMLAttributes<HTMLSelectElement> {
+  options: string[];
+  selectedOption?: string;
+  handleSelect: (value: string) => void;
+}
+
+const HiddenDropdown = ({ options, selectedOption, handleSelect }: HiddenDropdownProps) => {
+  return (
+    <select
+      style={{ display: 'none' }}
+      value={selectedOption || ''}
+      onChange={(event: React.ChangeEvent<HTMLSelectElement>) => handleSelect(event.target.value)}
+    >
+      {options.map((option) => (
+        <option key={option} value={option}>
+          {option}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+// 웹 접근성 향상을 위해 숨겨진 select 태그 구현
+
+export default HiddenDropdown;
