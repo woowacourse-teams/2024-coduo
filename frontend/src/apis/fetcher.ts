54cdd65aca1c6d56af6dbc837064d4a6ce9aa3dc
GimGayeon <kky1373@naver.com>
2024-07-23 22:47:41 +0900

    ✨ Fetcher 함수 구현
    api 요청 함수 추상화

---
+++ frontend/src/apis/fetcher.ts
@@ -0,0 +1,47 @@
+interface RequestProps {
+  url: string;
+  method: 'GET' | 'POST' | 'DELETE' | 'PATCH' | 'PUT';
+  errorMessage: string;
+  body?: Record<string, string | number>;
+  headers?: Record<string, string>;
+}
+
+type FetchProps = Omit<RequestProps, 'method'>;
+
+const fetcher = {
+  async request({ url, method, errorMessage, body, headers }: RequestProps): Promise<Response> {
+    try {
+      const response = await fetch(url, {
+        method,
+        body: body && JSON.stringify(body),
+        headers: headers && headers,
+      });
+
+      return response;
+    } catch (error) {
+      if (!(error instanceof Error)) {
+        throw new Error(errorMessage);
+      }
+
+      throw error;
+    }
+  },
+
+  get({ url, headers, errorMessage }: FetchProps) {
+    return this.request({ url, method: 'GET', headers, errorMessage });
+  },
+  post({ url, body, headers, errorMessage }: FetchProps) {
+    return this.request({ url, method: 'POST', body, headers, errorMessage });
+  },
+  delete({ url, headers, errorMessage }: FetchProps) {
+    return this.request({ url, method: 'DELETE', headers, errorMessage });
+  },
+  patch({ url, headers, errorMessage, body }: FetchProps) {
+    return this.request({ url, method: 'PATCH', headers, errorMessage, body });
+  },
+  put({ url, headers, errorMessage }: FetchProps) {
+    return this.request({ url, method: 'PUT', headers, errorMessage });
+  },
+};
+
+export default fetcher;
