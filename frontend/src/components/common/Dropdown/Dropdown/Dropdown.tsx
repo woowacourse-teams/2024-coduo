a7f2a710db4580b1f1cb63b1f7c5b2057b6bf77b
greetings1012 <ahnqwerty@gmail.com>
2024-07-23 15:46:02 +0900

    🚑️ 클릭 시 Dropdown 닫힘 기능 추가

--- frontend/src/components/common/Dropdown/Dropdown/Dropdown.tsx
+++ frontend/src/components/common/Dropdown/Dropdown/Dropdown.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import * as S from '@/components/common/Dropdown/Dropdown/Dropdown.styles';
 import HiddenDropdown from '@/components/common/Dropdown/HiddenDropdown/HiddenDropdown';
+
+import useClickOutside from '@/hooks/useClickOutside';
 
 import { theme } from '@/styles/theme';
 
@@ -10,12 +12,20 @@ interface DropdownProps {
   options: string[];
   onSelect: (option: string) => void;
   defaultOption?: string;
+  selected?: string;
   width?: string;
 }
 
-const Dropdown = ({ placeholder, options, onSelect, defaultOption, width = '100%' }: DropdownProps) => {
+const Dropdown = ({ placeholder, options, onSelect, defaultOption, selected, width = '100%' }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(defaultOption || '');
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (selected !== undefined) {
+      setSelectedOption(selected);
+    }
+  }, [selected]);
 
   const handleSelect = (option: string) => {
     setSelectedOption(option);
@@ -23,8 +33,10 @@ const Dropdown = ({ placeholder, options, onSelect, defaultOption, width = '100%
     setIsOpen(false);
   };
 
+  useClickOutside(dropdownRef, () => setIsOpen(false));
+
   return (
-    <S.Layout $width={width}>
+    <S.Layout $width={width} ref={dropdownRef}>
       <HiddenDropdown options={options} selectedOption={selectedOption} handleSelect={handleSelect} />
       <S.OpenButton
         filled={false}
