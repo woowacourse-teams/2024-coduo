8a05f23394d3795f2a5db4f02a04fc8f9e45fbfb
anttiey <hanuuny@gmail.com>
2024-07-23 14:11:33 +0900

    :lipstick: 전역 fontWeight 스타일 옵션 추가

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -97,11 +97,15 @@ const deviceWidth = {
 };
 
 const fontWeight = {
+  thin: '100',
+  extraLight: '200',
   light: '300',
-  base: '400',
-  medium: '600',
-  semibold: '700',
-  bold: '900',
+  normal: '400',
+  medium: '500',
+  semibold: '600',
+  bold: '700',
+  extraBold: '800',
+  black: '900',
 };
 
 const iconSize = {
