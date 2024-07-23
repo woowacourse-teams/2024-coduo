a0cf4866952f856cb43f030294b39d68b0a1914f
anttiey <hanuuny@gmail.com>
2024-07-23 16:38:28 +0900

    :lipstick: PairRoom 페이지 UI 구현

--- frontend/src/pages/PairRoom/PairRoom.styles.tsx
+++ frontend/src/pages/PairRoom/PairRoom.styles.tsx
@@ -1,6 +1,27 @@
 import styled from 'styled-components';
 
 export const Layout = styled.div`
+  display: flex;
+  gap: 3rem;
+  width: 100%;
   min-height: calc(100vh - 7rem);
+  padding: 3rem;
   background: ${({ theme }) => theme.color.primary[100]};
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  width: 41vw;
+`;
+
+export const PairListCard = styled.div`
+  display: flex;
+  width: 18vw;
+`;
+
+export const PairRoleCard = styled.div`
+  display: flex;
+  height: 16rem;
 `;
--- frontend/src/pages/PairRoom/PairRoom.tsx
+++ frontend/src/pages/PairRoom/PairRoom.tsx
@@ -1,7 +1,39 @@
+import { IoPeople } from 'react-icons/io5';
+
+import { PairRoomCard } from '@/components/PairRoom/PairRoomCard';
+
+import { theme } from '@/styles/theme';
+
 import * as S from './PairRoom.styles';
 
 const PairRoom = () => {
-  return <S.Layout>페어룸</S.Layout>;
+  return (
+    <S.Layout>
+      <S.PairListCard>
+        <PairRoomCard>
+          <PairRoomCard.Header icon={<IoPeople color={theme.color.primary[500]} />} title="페어" />
+        </PairRoomCard>
+      </S.PairListCard>
+      <S.Container>
+        <S.PairRoleCard>
+          <PairRoomCard>
+            <div>내용</div>
+          </PairRoomCard>
+        </S.PairRoleCard>
+        <PairRoomCard>
+          <div>내용</div>
+        </PairRoomCard>
+      </S.Container>
+      <S.Container>
+        <PairRoomCard>
+          <PairRoomCard.Header icon={<IoPeople color={theme.color.primary[500]} />} title="링크" />
+        </PairRoomCard>
+        <PairRoomCard>
+          <PairRoomCard.Header icon={<IoPeople color={theme.color.primary[500]} />} title="메모" />
+        </PairRoomCard>
+      </S.Container>
+    </S.Layout>
+  );
 };
 
 export default PairRoom;
