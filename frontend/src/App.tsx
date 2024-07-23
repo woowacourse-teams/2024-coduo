ca7e33f1967a134b20e78f8c30094e477581b5fe
greetings1012 <ahnqwerty@gmail.com>
2024-07-23 16:40:56 +0900

    🚑️ 버그 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,22 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
 import { ThemeProvider } from 'styled-components';
 
-import Layout from '@/pages/Layout';
-import Main from '@/pages/Main/Main';
+import Dropdown from '@/components/common/Dropdown/Dropdown/Dropdown';
+import { Modal } from '@/components/common/Modal';
 
 import GlobalStyles from './styles/Global.style';
 import { theme } from './styles/theme';
 
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path: '',
-          element: <Main />,
-        },
-      ],
-    },
-  ]);
-
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      <Modal isOpen={true} close={() => {}}>
+        <Dropdown
+          placeholder="선택해주세요"
+          options={['옵션1', '옵션2', '옵션3']}
+          onSelect={(value) => console.log(value)}
+        />
+      </Modal>
     </ThemeProvider>
   );
 };
