b7fa0b2a2de8c4b35b6a1a61bc0928e0b8e0d1e4
anttiey <hanuuny@gmail.com>
2024-07-24 10:59:27 +0900

    :lipstick: Storybook 추가

---
+++ frontend/src/components/PairRoom/PairListCard/PairListCard.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import PairListCard from './PairListCard';
+
+const meta = {
+  title: 'component/PairRoom/PairListCard',
+  component: PairListCard,
+} satisfies Meta<typeof PairListCard>;
+
+export default meta;
+
+type Story = StoryObj<typeof PairListCard>;
+
+export const Default: Story = {
+  args: {},
+};
