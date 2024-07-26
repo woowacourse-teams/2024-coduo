900db0477a38b9153b0db27ef5c887b675f791af
anttiey <hanuuny@gmail.com>
2024-07-26 10:38:27 +0900

    :sparkles: 페어 역할과 타이머 기능 연결

--- frontend/src/pages/PairRoom/PairRoom.tsx
+++ frontend/src/pages/PairRoom/PairRoom.tsx
@@ -1,45 +1,49 @@
 import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import MemoCard from '@/components/PairRoom/MemoCard/MemoCard';
-import OnboardingModal from '@/components/PairRoom/OnboardingModal/OnboardingModal';
 import PairListCard from '@/components/PairRoom/PairListCard/PairListCard';
 import PairRoleCard from '@/components/PairRoom/PairRoleCard/PairRoleCard';
 import ReferenceCard from '@/components/PairRoom/ReferenceCard/ReferenceCard';
 import TimerCard from '@/components/PairRoom/TimerCard/TimerCard';
 
-import useModal from '@/hooks/useModal';
+import useTimer from '@/hooks/PairRoom/useTimer';
 
 import * as S from './PairRoom.styles';
 
 const PairRoom = () => {
-  const { accessCode } = useParams();
+  const { state } = useLocation();
 
-  const [driver, setDriver] = useState('');
-  const [navigator, setNavigator] = useState('');
+  const [driver, setDriver] = useState(state.driver || '');
+  const [navigator, setNavigator] = useState(state.navigator || '');
+
+  const time = Number(state.timer || '') * 60 * 1000;
 
   const handleSwap = () => {
     setDriver(navigator);
     setNavigator(driver);
   };
 
-  const { isModalOpen, closeModal } = useModal(true);
+  const { timeLeft, isActive, handleStart, handlePause, handleStop } = useTimer(time, handleSwap);
 
   return (
-    <>
-      <S.Layout>
-        <PairListCard />
-        <S.Container>
-          <PairRoleCard driver={driver} navigator={navigator} onSwap={handleSwap} />
-          <TimerCard />
-        </S.Container>
-        <S.Container>
-          <ReferenceCard />
-          <MemoCard />
-        </S.Container>
-      </S.Layout>
-      <OnboardingModal accessCode={accessCode || ''} isOpen={isModalOpen} closeModal={closeModal} />
-    </>
+    <S.Layout>
+      <PairListCard />
+      <S.Container>
+        <PairRoleCard driver={driver} navigator={navigator} onSwap={handleSwap} onTimerReset={handleStop} />
+        <TimerCard
+          timeLeft={timeLeft}
+          isActive={isActive}
+          onStart={handleStart}
+          onPause={handlePause}
+          onStop={handleStop}
+        />
+      </S.Container>
+      <S.Container>
+        <ReferenceCard />
+        <MemoCard />
+      </S.Container>
+    </S.Layout>
   );
 };
 
