e0ed9fa1463e38fa3024ba469b79b0e3efca451b
Younghoon Ahn(Paran) <ahnqwerty@gmail.com>
2024-07-30 09:25:53 +0000

    ♻️ 필요없는 onCopy 반환값 제거

--- frontend/src/hooks/useCopyClipboard.ts
+++ frontend/src/hooks/useCopyClipboard.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-type onCopyFn = (text: string) => Promise<boolean>;
+type onCopyFn = (text: string) => Promise<void>;
 
 const useCopyClipBoard = (): [boolean, onCopyFn] => {
   const [isCopy, setIsCopy] = useState<boolean>(false);
@@ -10,13 +10,10 @@ const useCopyClipBoard = (): [boolean, onCopyFn] => {
       await navigator.clipboard.writeText(text);
       setIsCopy(true);
       alert('클립보드에 복사되었습니다.');
-      return true;
     } catch (error) {
       console.error(error);
       setIsCopy(false);
       alert('클립보드 복사에 실패했습니다.');
-
-      return false;
     }
   };
 
