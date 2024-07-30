1f10dbda210707422819455154b3856c12dcac09
GimGayeon <kky1373@naver.com>
2024-07-31 00:06:51 +0900

    ♻️ UseTimer 훅 함수 분리 및 리팩터링

--- frontend/src/hooks/PairRoom/useTimer.ts
+++ frontend/src/hooks/PairRoom/useTimer.ts
@@ -2,17 +2,20 @@ import { useRef, useState, useEffect } from 'react';
 
 const useTimer = (defaultTime: number, onStop: () => void) => {
   const timerRef = useRef<NodeJS.Timeout | null>(null);
-
   const [timeLeft, setTimeLeft] = useState(defaultTime);
   const [isActive, setIsActive] = useState(false);
   const [startTime, setStartTime] = useState<number | null>(null);
 
   const handleStart = () => {
-    setStartTime(Date.now());
-    setIsActive(true);
+    if (!isActive) {
+      setStartTime(Date.now() - (defaultTime - timeLeft));
+      setIsActive(true);
+    }
   };
 
-  const handlePause = () => setIsActive(false);
+  const handlePause = () => {
+    setIsActive(false);
+  };
 
   const handleStop = () => {
     setIsActive(false);
@@ -21,19 +24,22 @@ const useTimer = (defaultTime: number, onStop: () => void) => {
   };
 
   useEffect(() => {
+    const notifyTimerEnd = () => {
+      setIsActive(false);
+      setTimeLeft(0);
+      onStop();
+      return;
+    };
+
     if (isActive && timeLeft > 0) {
       timerRef.current = setInterval(() => {
         if (!startTime) return;
 
         const elapsedTime = Date.now() - startTime;
         const newTimeLeft = defaultTime - elapsedTime;
-        if (newTimeLeft <= 0) {
-          setIsActive(false);
-          setTimeLeft(0);
-          onStop();
-        } else {
-          setTimeLeft(newTimeLeft);
-        }
+
+        if (newTimeLeft <= 0) notifyTimerEnd();
+        setTimeLeft(newTimeLeft);
       }, 10);
 
       return () => {
@@ -50,7 +56,7 @@ const useTimer = (defaultTime: number, onStop: () => void) => {
     return () => {
       if (timerRef.current) clearInterval(timerRef.current);
     };
-  }, [isActive, startTime, defaultTime, onStop, timeLeft]);
+  }, [isActive, startTime, timeLeft]);
 
   return { timeLeft, isActive, handleStart, handlePause, handleStop };
 };
