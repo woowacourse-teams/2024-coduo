042f5632e02155c41cf46d7ab88f4f2c1b26efc6
greetings1012 <ahnqwerty@gmail.com>
2024-07-31 16:49:25 +0900

    💄 스토리북에서 QueryClient, Theme, Router preview.tsx에 한 번에 설정

--- frontend/.storybook/preview.tsx
+++ frontend/.storybook/preview.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import type { Preview } from '@storybook/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 import { theme } from '../src/styles/theme';
-import GlobalStyles from '../src/styles/Global.style';
+import GlobalStyle from '../src/styles/Global.style';
+
+const queryClient = new QueryClient();
+
 const preview: Preview = {
   parameters: {
     viewport: {
@@ -17,11 +22,16 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <Story />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </BrowserRouter>
     ),
   ],
 };
+
 export default preview;
