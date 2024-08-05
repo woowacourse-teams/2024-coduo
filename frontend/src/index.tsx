78799f047d4be3ebbfaa53479ad87cd69c825984
greetings1012 <ahnqwerty@gmail.com>
2024-08-05 15:52:25 +0900

    📈 Sentry 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import * as Sentry from '@sentry/react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
 import './styles/font.css';
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [/^https:\/\/coduo\.site\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
