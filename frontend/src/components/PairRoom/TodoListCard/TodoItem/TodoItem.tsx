22cab8d04b663ec189dfe3b329cced7070dd585f
anttiey <hanuuny@gmail.com>
2024-08-06 15:46:31 +0900

    :sparkles: TodoItem 드래그 앤 드롭 기능 추가

--- frontend/src/components/PairRoom/TodoListCard/TodoItem/TodoItem.tsx
+++ frontend/src/components/PairRoom/TodoListCard/TodoItem/TodoItem.tsx
@@ -3,12 +3,22 @@ import CheckBox from '@/components/common/CheckBox/CheckBox';
 import * as S from './TodoItem.styles';
 
 interface TodoItemProps {
+  position: number;
   content: string;
+  onDragStart: (position: number) => void;
+  onDragEnter: (position: number) => void;
+  onDrop: (event: React.DragEvent<HTMLDivElement>) => void;
 }
 
-const TodoItem = ({ content }: TodoItemProps) => {
+const TodoItem = ({ position, content, onDragStart, onDragEnter, onDrop }: TodoItemProps) => {
   return (
-    <S.Layout>
+    <S.Layout
+      draggable
+      onDragStart={() => onDragStart(position)}
+      onDragEnter={() => onDragEnter(position)}
+      onDragOver={(event) => event.preventDefault()}
+      onDrop={onDrop}
+    >
       <CheckBox />
       <p>{content}</p>
     </S.Layout>
--- frontend/src/components/PairRoom/TodoListCard/TodoListCard.tsx
+++ frontend/src/components/PairRoom/TodoListCard/TodoListCard.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+
 import { IoIosCheckbox } from 'react-icons/io';
 
 import { PairRoomCard } from '@/components/PairRoom/PairRoomCard';
@@ -8,15 +10,47 @@ import { theme } from '@/styles/theme';
 import * as S from './TodoListCard.styles';
 
 const TodoListCard = () => {
+  const [todos, setTodos] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']);
+
+  const dragItem = useRef<number | null>(null);
+  const dragOverItem = useRef<number | null>(null);
+
+  const handleDragStart = (position: number) => (dragItem.current = position);
+
+  const handleDragEnter = (position: number) => (dragOverItem.current = position);
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
+    if (dragItem.current === null || dragOverItem.current === null) return;
+
+    const newTodos = [...todos];
+    console.log(dragItem.current, newTodos[dragItem.current]);
+    const draggedItem = newTodos[dragItem.current];
+
+    newTodos.splice(dragItem.current, 1);
+    newTodos.splice(dragOverItem.current, 0, draggedItem);
+
+    dragItem.current = null;
+    dragOverItem.current = null;
+
+    setTodos(newTodos);
+  };
+
   return (
     <PairRoomCard>
       <PairRoomCard.Header icon={<IoIosCheckbox color={theme.color.primary[500]} />} title="투두 리스트" />
       <S.TodoListWrapper>
-        <TodoItem content="Item 1" />
-        <TodoItem content="Item 2" />
-        <TodoItem content="Item 3" />
-        <TodoItem content="Item 4" />
-        <TodoItem content="Item 5" />
+        {todos.map((todo, idx) => (
+          <TodoItem
+            key={idx}
+            position={idx}
+            content={todo}
+            onDragStart={handleDragStart}
+            onDragEnter={handleDragEnter}
+            onDrop={handleDrop}
+          />
+        ))}
       </S.TodoListWrapper>
     </PairRoomCard>
   );
