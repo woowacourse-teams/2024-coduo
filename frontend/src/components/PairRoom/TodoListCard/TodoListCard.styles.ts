f9eaa5bf929ab33382474049555c5ed93a0beb2c
anttiey <hanuuny@gmail.com>
2024-08-06 16:42:22 +0900

    :sparkles: TodoItem 추가 기능 추가

--- frontend/src/components/PairRoom/TodoListCard/TodoListCard.styles.ts
+++ frontend/src/components/PairRoom/TodoListCard/TodoListCard.styles.ts
@@ -6,3 +6,50 @@ export const TodoListWrapper = styled.div`
   gap: 1rem;
   padding: 3rem;
 `;
+
+export const Footer = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 7rem;
+  max-height: 7rem;
+
+  border-top: 1px solid ${({ theme }) => theme.color.black[30]};
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  gap: 3rem;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  padding: 0 2rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 0.6rem;
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+
+  width: 100%;
+  height: 7rem;
+  padding: 2rem;
+
+  font-size: ${({ theme }) => theme.fontSize.base};
+  color: ${({ theme }) => theme.color.black[70]};
+
+  border-radius: 0 0 1.5rem 1.5rem;
+
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.black[20]};
+  }
+`;
--- frontend/src/components/PairRoom/TodoListCard/TodoListCard.tsx
+++ frontend/src/components/PairRoom/TodoListCard/TodoListCard.tsx
@@ -1,16 +1,24 @@
 import { useState, useRef } from 'react';
 
 import { IoIosCheckbox } from 'react-icons/io';
+import { LuPlus } from 'react-icons/lu';
 
+import Button from '@/components/common/Button/Button';
+import Input from '@/components/common/Input/Input';
 import { PairRoomCard } from '@/components/PairRoom/PairRoomCard';
 import TodoItem from '@/components/PairRoom/TodoListCard/TodoItem/TodoItem';
+
+import useInput from '@/hooks/common/useInput';
 
 import { theme } from '@/styles/theme';
 
 import * as S from './TodoListCard.styles';
 
 const TodoListCard = () => {
+  const [isInputOpen, setIsInputOpen] = useState(false);
   const [todos, setTodos] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']);
+
+  const { value, handleChange, resetValue } = useInput();
 
   const dragItem = useRef<number | null>(null);
   const dragOverItem = useRef<number | null>(null);
@@ -37,6 +45,13 @@ const TodoListCard = () => {
     setTodos(newTodos);
   };
 
+  const addTodoItem = () => {
+    setTodos((prev) => [...prev, value]);
+
+    resetValue();
+    setIsInputOpen(false);
+  };
+
   return (
     <PairRoomCard>
       <PairRoomCard.Header icon={<IoIosCheckbox color={theme.color.primary[500]} />} title="투두 리스트" />
@@ -52,6 +67,26 @@ const TodoListCard = () => {
           />
         ))}
       </S.TodoListWrapper>
+      <S.Footer>
+        {isInputOpen ? (
+          <S.InputContainer>
+            <Input value={value} onChange={handleChange} />
+            <S.ButtonContainer>
+              <Button size="sm" filled={false} rounded={true} onClick={() => setIsInputOpen(false)}>
+                취소
+              </Button>
+              <Button size="sm" rounded={true} onClick={addTodoItem}>
+                확인
+              </Button>
+            </S.ButtonContainer>
+          </S.InputContainer>
+        ) : (
+          <S.AddButton onClick={() => setIsInputOpen(true)}>
+            <LuPlus />
+            투두 추가하기
+          </S.AddButton>
+        )}
+      </S.Footer>
     </PairRoomCard>
   );
 };
