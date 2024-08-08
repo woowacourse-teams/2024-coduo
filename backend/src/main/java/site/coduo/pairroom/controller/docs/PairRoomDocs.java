e33c7ec7b7f3bbb6abb892c3df7c53e801846813
reddevilmidzy <midzy0228@gmail.com>
2024-08-08 14:46:45 +0900

    docs: 페어룸 API 문서 작업

--- backend/src/main/java/site/coduo/pairroom/controller/docs/PairRoomDocs.java
+++ backend/src/main/java/site/coduo/pairroom/controller/docs/PairRoomDocs.java
@@ -13,6 +13,8 @@ import site.coduo.common.controller.response.ApiErrorResponse;
 import site.coduo.pairroom.dto.PairRoomCreateRequest;
 import site.coduo.pairroom.dto.PairRoomCreateResponse;
 import site.coduo.pairroom.dto.PairRoomDeleteRequest;
+import site.coduo.pairroom.dto.PairRoomExistRequest;
+import site.coduo.pairroom.dto.PairRoomExistResponse;
 import site.coduo.pairroom.dto.PairRoomReadRequest;
 import site.coduo.pairroom.dto.PairRoomReadResponse;
 
@@ -27,7 +29,9 @@ public interface PairRoomDocs {
             content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
                     schema = @Schema(implementation = ApiErrorResponse.class)))
     ResponseEntity<PairRoomReadResponse> getPairRoom(
-            @Parameter(description = "페어룸 접근 코드", required = true) PairRoomReadRequest accessCode);
+            @Parameter(description = "페어룸 접근 코드", required = true)
+            PairRoomReadRequest accessCode
+    );
 
     @Operation(summary = "페어룸을 생성한다.")
     @ApiResponse(responseCode = "201", description = "페어룸 생성 성공",
@@ -37,8 +41,20 @@ public interface PairRoomDocs {
             @Parameter(description = "페어 프로그래밍에 참여하는 페어 A의 이름, 페어 B의 이름", required = true)
             PairRoomCreateRequest pairRoomCreateRequest);
 
+    @Operation(summary = "페어룸이 존재하는지 확인한다.")
+    @ApiResponse(responseCode = "200", description = "페어룸 존재 여부 확인 성공",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = PairRoomExistResponse.class)))
+    ResponseEntity<PairRoomExistResponse> existPairRoom(
+            @Parameter(description = "페어룸 액세스 코드", required = true)
+            PairRoomExistRequest request
+    );
+
     @Operation(summary = "페어룸을 삭제한다.")
     @ApiResponse(responseCode = "204", description = "페어룸 삭제 성공")
     @ApiResponse(responseCode = "404", description = "페어룸 삭제 실패")
-    ResponseEntity<Void> deletePairRoom(@Parameter(description = "페어룸 접근 코드") PairRoomDeleteRequest request);
+    ResponseEntity<Void> deletePairRoom(
+            @Parameter(description = "페어룸 접근 코드", required = true)
+            PairRoomDeleteRequest request
+    );
 }
