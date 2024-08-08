096b12ed9b71c5c632903e5dd5394cb8647e9bab
GimGayeon <kky1373@naver.com>
2024-08-08 12:42:41 +0900

    ✨ Ga 경로 추적 훅 구현

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -3,9 +3,12 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/components/common/Header/Header';
 import ToastList from '@/components/common/ToastList/ToastList';
 
+import useRouteChangeTracker from '@/hooks/useRouteChangeTracker';
+
 import * as S from './Layout.styles';
 
 const Layout = () => {
+  useRouteChangeTracker();
   return (
     <S.Layout>
       <Header />
