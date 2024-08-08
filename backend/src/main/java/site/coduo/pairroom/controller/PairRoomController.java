a6c629b26d2ae7a2ab74d431fc3aa7a24210ca1d
reddevilmidzy <midzy0228@gmail.com>
2024-08-08 22:23:02 +0900

    refactor: API prefix 설정 requestMapping 대신 yml 파일로 설정

--- backend/src/main/java/site/coduo/pairroom/controller/PairRoomController.java
+++ backend/src/main/java/site/coduo/pairroom/controller/PairRoomController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -24,7 +23,6 @@ import site.coduo.pairroom.dto.PairRoomReadResponse;
 import site.coduo.pairroom.service.PairRoomService;
 
 @RequiredArgsConstructor
-@RequestMapping("/api")
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "https://coduo.site"})
 public class PairRoomController implements PairRoomDocs {
--- backend/src/main/java/site/coduo/referencelink/controller/ReferenceLinkController.java
+++ backend/src/main/java/site/coduo/referencelink/controller/ReferenceLinkController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,6 @@ import site.coduo.referencelink.service.dto.ReferenceLinkCreateRequest;
 import site.coduo.referencelink.service.dto.ReferenceLinkResponse;
 
 @RequiredArgsConstructor
-@RequestMapping("/api")
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "https://coduo.site"})
 public class ReferenceLinkController implements ReferenceLinkDocs {
