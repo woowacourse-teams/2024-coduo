63f6ce312d3e08e9babd8334a8f365e9a865b7f0
greetings1012 <ahnqwerty@gmail.com>
2024-08-10 23:31:24 +0900

    ✨ 404 에러 페이지 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 
+import PageNotFound from '@/pages/Error/PageNotFound';
 import HowToPair from '@/pages/HowToPair/HowToPair';
 import Layout from '@/pages/Layout';
 import Main from '@/pages/Main/Main';
@@ -35,6 +36,10 @@ const App = () => {
         {
           path: 'room/:accessCode/onboarding',
           element: <PairRoomOnboarding />,
+        },
+        {
+          path: '*',
+          element: <PageNotFound />,
         },
       ],
     },
