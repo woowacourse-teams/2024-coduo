5fa7e089c4556f9a02224fa47d5ca35a49751587
fram1998 <koust6u@pusan.ac.kr>
2024-08-15 01:23:04 +0900

    chore: 로그 추가

--- backend/src/main/java/site/coduo/member/service/MemberService.java
+++ backend/src/main/java/site/coduo/member/service/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import site.coduo.member.client.GithubApiClient;
 import site.coduo.member.client.dto.GithubUserRequest;
 import site.coduo.member.client.dto.GithubUserResponse;
@@ -11,6 +12,7 @@ import site.coduo.member.domain.Member;
 import site.coduo.member.domain.repository.MemberRepository;
 import site.coduo.member.infrastructure.http.Bearer;
 
+@Slf4j
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Service
@@ -24,6 +26,12 @@ public class MemberService {
         final Bearer bearer = new Bearer(accessToken);
         GithubUserResponse userResponse = githubClient.getUser(new GithubUserRequest(bearer));
         Member member = userResponse.toDomain(bearer, username);
+        log.warn("member access token: {}", member.getAccessToken());
+        log.warn("member username: {}", member.getUsername());
+        log.warn("member userId: {}", member.getUserId());
+        log.warn("member loginId: {}", member.getLoginId());
+        log.warn("member profile photo: {}", member.getProfileImage());
+
         memberRepository.save(member);
     }
 }
