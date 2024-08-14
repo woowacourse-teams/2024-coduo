34cab13bd24de8f0828fac38a9c5ca8a69bb1aaa
김민종 <koust6u@pusan.ac.kr>
2024-08-15 01:50:54 +0900

    [BE] Jackson 파싱 에러 수정  (#386)
    * chore: 로그 추가
    
    * fix: Jackson 파싱 에러 수정
    
    * fix: Jackson 파싱 에러 수정

--- backend/src/main/java/site/coduo/member/client/dto/GithubUserResponse.java
+++ backend/src/main/java/site/coduo/member/client/dto/GithubUserResponse.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import site.coduo.member.domain.Member;
 import site.coduo.member.infrastructure.http.Bearer;
 
-public record GithubUserResponse(@JsonProperty(value = "user_id") String userId,
+public record GithubUserResponse(@JsonProperty(value = "id") String userId,
                                  @JsonProperty(value = "login") String longin,
                                  @JsonProperty(value = "avatar_url") String avatarUrl
 ) {
