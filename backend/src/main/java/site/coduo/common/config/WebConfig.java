18a2c1e286f7442c5b838a7c95b7ca49a22a2a8e
reddevilmidzy <midzy0228@gmail.com>
2024-08-16 17:17:01 +0900

    fix: 스웨거 api prefix 롤백

--- backend/src/main/java/site/coduo/common/config/WebConfig.java
+++ backend/src/main/java/site/coduo/common/config/WebConfig.java
@@ -1,7 +1,6 @@
 package site.coduo.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,8 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class))
-                .addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RequestMapping.class));
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
     }
 
     @Override
