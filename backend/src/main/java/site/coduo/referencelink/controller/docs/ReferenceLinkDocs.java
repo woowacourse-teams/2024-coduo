3b9d11be07aa3f66edfbe53dca2e9fc5950c740c
lemone <lejinhy@gmail.com>
2024-08-16 14:35:47 +0900

    docs: 문서화 잘못된 부분 수정 & 구체화

--- backend/src/main/java/site/coduo/referencelink/controller/docs/ReferenceLinkDocs.java
+++ backend/src/main/java/site/coduo/referencelink/controller/docs/ReferenceLinkDocs.java
@@ -19,7 +19,7 @@ import site.coduo.referencelink.service.dto.ReferenceLinkResponse;
 public interface ReferenceLinkDocs {
 
     @Operation(summary = "레퍼런스 링크를 생성한다.")
-    @ApiResponse(responseCode = "200", description = "레퍼런스 링크 생성 성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ReferenceLinkCreateRequest.class)))
+    @ApiResponse(responseCode = "201", description = "레퍼런스 링크 생성 성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
     @ApiResponse(responseCode = "4xx", description = "레퍼런스 링크 생성 실패", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class)))
     ResponseEntity<Void> createReferenceLink(
             String accessCode,
@@ -32,7 +32,7 @@ public interface ReferenceLinkDocs {
     ResponseEntity<List<ReferenceLinkResponse>> getReferenceLinks(String accessCode);
 
     @Operation(summary = "레퍼런스 링크를 삭제한다.")
-    @ApiResponse(responseCode = "200", description = "레퍼런스 링크 삭제 성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
+    @ApiResponse(responseCode = "204", description = "레퍼런스 링크 삭제 성공", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))
     @ApiResponse(responseCode = "4xx", description = "레퍼런스 링크 삭제 실패", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ApiErrorResponse.class)))
     ResponseEntity<Void> deleteReferenceLink(
             String accessCode,
