4da15b0d730aa5ba654e33e58e05316f259471ef
lemone <lejinhy@gmail.com>
2024-08-16 15:18:11 +0900

    refactor: api prefix 전역처리

--- backend/src/main/java/site/coduo/pairroomhistory/controller/PairRoomHistoryController.java
+++ backend/src/main/java/site/coduo/pairroomhistory/controller/PairRoomHistoryController.java
@@ -3,12 +3,10 @@ package site.coduo.pairroomhistory.controller;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +16,7 @@ import site.coduo.pairroomhistory.dto.PairRoomHistoryReadResponse;
 import site.coduo.pairroomhistory.service.PairRoomHistoryService;
 
 @RequiredArgsConstructor
-@RequestMapping("/api")
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000", "https://coduo.site"})
 public class PairRoomHistoryController implements PairRoomHistoryDocs {
 
     private final PairRoomHistoryService pairRoomHistoryService;
