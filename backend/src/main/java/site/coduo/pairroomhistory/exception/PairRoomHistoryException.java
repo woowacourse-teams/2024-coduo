99fa2db08231b87191a523bba8526990bd59ac6a
lemone <lejinhy@gmail.com>
2024-08-16 16:35:17 +0900

    refactor: 클래스 선언 다음 공백 추가

--- backend/src/main/java/site/coduo/pairroomhistory/exception/PairRoomHistoryException.java
+++ backend/src/main/java/site/coduo/pairroomhistory/exception/PairRoomHistoryException.java
@@ -1,6 +1,7 @@
 package site.coduo.pairroomhistory.exception;
 
 public class PairRoomHistoryException extends RuntimeException {
+
     public PairRoomHistoryException(final String message) {
         super(message);
     }
