b532bc600fcad50206ee6780848c26534d266bc3
reddevilmidzy <midzy0228@gmail.com>
2024-08-16 16:58:32 +0900

    fix: 스웨거 api prefix 추가

--- backend/src/main/java/site/coduo/common/config/WebConfig.java
+++ backend/src/main/java/site/coduo/common/config/WebConfig.java
@@ -1,6 +1,7 @@
 package site.coduo.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -15,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configurePathMatch(PathMatchConfigurer configurer) {
-        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class))
+                .addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RequestMapping.class));
     }
 
     @Override
