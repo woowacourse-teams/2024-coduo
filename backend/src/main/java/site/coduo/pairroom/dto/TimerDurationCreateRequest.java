0e44adffdaca072bf2c776c05213cf6512fd7087
lemone <lejinhy@gmail.com>
2024-08-17 19:34:40 +0900

    feat: 페어룸 히스토리 문서화 추가

--- backend/src/main/java/site/coduo/pairroom/dto/TimerDurationCreateRequest.java
+++ backend/src/main/java/site/coduo/pairroom/dto/TimerDurationCreateRequest.java
@@ -1,10 +1,13 @@
 package site.coduo.pairroom.dto;
 
+import jakarta.validation.constraints.NotNull;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "타이머 시간 저장 요청 바디")
 public record TimerDurationCreateRequest(
         @Schema(description = "타이머 시간. millisecond 기준으로 저장한다.", example = "60000")
+        @NotNull
         long timerDuration
 ) {
 }
--- backend/src/main/java/site/coduo/pairroomhistory/dto/PairRoomHistoryUpdateRequest.java
+++ backend/src/main/java/site/coduo/pairroomhistory/dto/PairRoomHistoryUpdateRequest.java
@@ -1,4 +1,13 @@
 package site.coduo.pairroomhistory.dto;
 
-public record PairRoomHistoryUpdateRequest(long timerRemainingTime) {
+import jakarta.validation.constraints.NotNull;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "페어룸 최근 히스토리 타이머 남은 시간 업데이트 요청 바디")
+public record PairRoomHistoryUpdateRequest(
+        @Schema(description = "타이머가 종료되기까지 남은 시간. 이 시간으로 페어룸 최근 히스토리가 업데이트 된다.", example = "60000")
+        @NotNull
+        long timerRemainingTime
+) {
 }
