832830fea55535dbb041e41500bc2a74f3726087
GimGayeon <kky1373@naver.com>
2024-08-18 22:06:22 +0900

    ♻️ UseInput 훅 resetValue 수정
    message, status 도 리셋되도록 수정

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,7 +1,9 @@
 import AlarmSound from '@/assets/audio/alarm_sound.mp3';
+import CheckBoxChecked from '@/assets/images/check_box_checked.svg';
+import CheckBoxUnchecked from '@/assets/images/check_box_unchecked.svg';
 import LogoIcon from '@/assets/images/logo_icon.svg';
 import LogoIconWithTitle from '@/assets/images/logo_icon_with_title.svg';
 import LogoTitle from '@/assets/images/logo_title.svg';
 import Wave from '@/assets/images/wave.svg';
 
-export { LogoIcon, LogoIconWithTitle, LogoTitle, Wave, AlarmSound };
+export { LogoIcon, CheckBoxUnchecked, LogoIconWithTitle, LogoTitle, Wave, AlarmSound, CheckBoxChecked };
