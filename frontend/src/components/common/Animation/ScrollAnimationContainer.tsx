4410401e55f3e299244d268990d9eb787d75a5a7
Younghoon Ahn(Paran) <ahnqwerty@gmail.com>
2024-08-19 19:42:14 +0900

    ♻️ 필요 없는 interface 제거

--- frontend/src/components/common/Animation/ScrollAnimationContainer.tsx
+++ frontend/src/components/common/Animation/ScrollAnimationContainer.tsx
@@ -2,11 +2,7 @@ import { useScrollAnimation } from '@/hooks/animations/useScrollAnimation';
 
 import * as S from './ScrollAnimationContainer.styles';
 
-interface PropsType {
-  children: React.ReactNode;
-}
-
-export const ScrollAnimationContainer = ({ children }: PropsType) => {
+export const ScrollAnimationContainer = ({ children }: React.PropsWithChildren) => {
   const { ref, isInViewport, hasAnimated } = useScrollAnimation();
   return (
     <S.Container ref={ref} className={isInViewport ? 'frame-in' : ''} $hasAnimated={hasAnimated}>
