8822d41323fec0acbde95a9656b6a8e859170a5c
reddevilmidzy <midzy0228@gmail.com>
2024-08-19 15:29:55 +0900

    feat: TodoErrorController 생성

---
+++ backend/src/main/java/site/coduo/todo/controller/TodoErrorController.java
@@ -0,0 +1,26 @@
+package site.coduo.todo.controller;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import lombok.extern.slf4j.Slf4j;
+import site.coduo.common.controller.response.ApiErrorResponse;
+import site.coduo.todo.controller.error.TodoApiError;
+import site.coduo.todo.domain.exception.TodoException;
+
+@Slf4j
+@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class TodoErrorController {
+
+    @ExceptionHandler(TodoException.class)
+    public ResponseEntity<ApiErrorResponse> handleTodoException(final TodoException e) {
+        log.warn(e.getMessage());
+
+        return ResponseEntity.status(TodoApiError.INVALID_TODO_REQUEST.getHttpStatus())
+                .body(new ApiErrorResponse(e.getMessage()));
+    }
+}
