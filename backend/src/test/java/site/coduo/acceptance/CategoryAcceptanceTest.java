ddfc31ef17287d33ad6ec3766343d9e3be5f7aa9
reddevilmidzy <midzy0228@gmail.com>
2024-08-20 13:24:00 +0900

    test: CategoryAcceptanceTest 생성

--- backend/src/test/java/site/coduo/acceptance/CategoryAcceptanceTest.java
+++ backend/src/test/java/site/coduo/acceptance/CategoryAcceptanceTest.java
@@ -1,5 +1,7 @@
 package site.coduo.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -10,6 +12,8 @@ import site.coduo.pairroom.dto.PairRoomCreateRequest;
 import site.coduo.pairroom.dto.PairRoomCreateResponse;
 import site.coduo.referencelink.service.dto.CategoryCreateRequest;
 import site.coduo.referencelink.service.dto.CategoryCreateResponse;
+import site.coduo.referencelink.service.dto.CategoryUpdateRequest;
+import site.coduo.referencelink.service.dto.CategoryUpdateResponse;
 
 @Transactional
 class CategoryAcceptanceTest extends AcceptanceFixture {
@@ -52,5 +56,64 @@ class CategoryAcceptanceTest extends AcceptanceFixture {
                 .log()
                 .all()
                 .statusCode(200);
+    }
+
+    @Test
+    @DisplayName("카테고리를 업데이트 한다.")
+    void update_category() {
+        //given
+        final PairRoomCreateResponse pairRoomUrl = PairRoomAcceptanceTest.createPairRoom(
+                new PairRoomCreateRequest("레디", "프람"));
+
+        createCategory(pairRoomUrl.accessCode(), new CategoryCreateRequest("이전 카테고리"));
+
+        final String updateName = "변경된 카테고리";
+        final CategoryUpdateRequest request = new CategoryUpdateRequest("이전 카테고리", updateName);
+
+        //when & then
+        final CategoryUpdateResponse categoryUpdateResponse = RestAssured
+                .given()
+                .log()
+                .all()
+                .contentType("application/json")
+
+                .when()
+                .body(request)
+                .patch("/api/{accessCode}/category", pairRoomUrl.accessCode())
+
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .extract()
+                .as(CategoryUpdateResponse.class);
+
+        assertThat(categoryUpdateResponse.updatedCategoryName()).isEqualTo(updateName);
+    }
+
+    @Test
+    @DisplayName("카테고리 삭제에 성공한다.")
+    void delete_category() {
+        //given
+        final PairRoomCreateResponse pairRoomUrl = PairRoomAcceptanceTest.createPairRoom(
+                new PairRoomCreateRequest("레디", "프람"));
+
+        final String categoryName = "자바";
+        createCategory(pairRoomUrl.accessCode(), new CategoryCreateRequest(categoryName));
+
+        //when & then
+        RestAssured
+                .given()
+                .log()
+                .all()
+                .contentType("application/json")
+
+                .when()
+                .delete("/api/{accessCode}/category/{categoryName}", pairRoomUrl.accessCode(), categoryName)
+
+                .then()
+                .log()
+                .all()
+                .statusCode(204);
     }
 }
