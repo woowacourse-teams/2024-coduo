81c17cd5a7d2cf3e2218c509158d3b6e50e95293
reddevilmidzy <midzy0228@gmail.com>
2024-08-20 10:49:28 +0900

    style: 불필요한 import 제거

--- backend/src/test/java/site/coduo/utils/CascadeCleaner.java
+++ backend/src/test/java/site/coduo/utils/CascadeCleaner.java
@@ -1,6 +1,5 @@
 package site.coduo.utils;
 
-import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
