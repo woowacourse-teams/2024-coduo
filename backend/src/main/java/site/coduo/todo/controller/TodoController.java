1a653b58c0eff9002b29a6c370602d688e11f592
reddevilmidzy <midzy0228@gmail.com>
2024-08-21 13:24:35 +0900

    refactor: 중복되던 api prefix 제거

--- backend/src/main/java/site/coduo/todo/controller/TodoController.java
+++ backend/src/main/java/site/coduo/todo/controller/TodoController.java
@@ -3,14 +3,12 @@ package site.coduo.todo.controller;
 import java.util.List;
 import java.util.stream.IntStream;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -23,8 +21,6 @@ import site.coduo.todo.domain.Todo;
 import site.coduo.todo.service.TodoService;
 
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:3000", "http://3.35.178.58"})
-@RequestMapping("/api")
 @RestController
 public class TodoController implements TodoDocs {
 
