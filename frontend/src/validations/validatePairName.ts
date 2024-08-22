01ad7360c31deed7d8b931fab973b9274131bd97
greetings1012 <ahnqwerty@gmail.com>
2024-08-22 18:16:04 +0900

    🚑 공백을 제외하고 페어 이름을 검증하도록 수정

--- frontend/src/validations/validatePairName.ts
+++ frontend/src/validations/validatePairName.ts
@@ -1,7 +1,7 @@
 import type { InputStatus } from '@/components/common/Input/Input.type';
 
 export const validateName = (name: string) => {
-  if (name === '') return { status: 'ERROR' as InputStatus, message: '값을 입력해주세요.' };
+  if (name.trim() === '') return { status: 'ERROR' as InputStatus, message: '값을 입력해주세요.' };
   if (name.length > 10)
     return { status: 'ERROR' as InputStatus, message: '이름(또는 닉네임)은 10자 이하로 입력해주세요.' };
 
@@ -9,7 +9,7 @@ export const validateName = (name: string) => {
 };
 
 export const validateDuplicateName = (firstPairName: string, secondPairName: string) => {
-  if (firstPairName !== '' && secondPairName !== '' && firstPairName === secondPairName)
+  if (firstPairName.trim() !== '' && secondPairName.trim() !== '' && firstPairName.trim() === secondPairName.trim())
     return { status: 'ERROR' as InputStatus, message: '중복된 이름(또는 닉네임)입니다. ' };
 
   return { status: 'DEFAULT' as InputStatus, message: '' };
