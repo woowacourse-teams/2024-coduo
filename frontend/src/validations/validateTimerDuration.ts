772617f5f02f4b55f081583c08f329d5f2238956
anttiey <hanuuny@gmail.com>
2024-08-23 10:05:04 +0900

    :ambulance: 타이머 시간에 문자열이 들어가는 문제 해결

--- frontend/src/validations/validateTimerDuration.ts
+++ frontend/src/validations/validateTimerDuration.ts
@@ -1,5 +1,5 @@
 export const validateTimerDuration = (timerDuration: string) => {
-  if (Number.isNaN(timerDuration)) return false;
+  if (Number.isNaN(Number(timerDuration))) return false;
   if (Number(timerDuration) <= 0) return false;
 
   return true;
