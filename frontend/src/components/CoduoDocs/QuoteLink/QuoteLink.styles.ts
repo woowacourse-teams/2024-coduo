44a9c4fe46e351ea332204ddeef2d850ec4972df
GimGayeon <kky1373@naver.com>
2024-09-11 11:18:22 +0900

    ✨ 인용 링크 컴포넌트 구현

---
+++ frontend/src/components/CoduoDocs/QuoteLink/QuoteLink.styles.ts
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+
+  gap: 0.8rem;
+`;
+
+export const QuoteBar = styled.span`
+  color: ${({ theme }) => theme.color.black[50]};
+  font-size: ${({ theme }) => theme.fontSize.h5};
+  font-weight: ${({ theme }) => theme.fontWeight.extraBold};
+`;
+
+export const Content = styled.p`
+  color: ${({ theme }) => theme.color.black[90]};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: ${({ theme }) => theme.fontWeight.extraLight};
+`;
+
+export const TextLink = styled(Link)`
+  color: ${({ theme }) => theme.color.primary[700]};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: ${({ theme }) => theme.fontWeight.semibold};
+
+  text-decoration: underline;
+
+  transition: color 0.2s ease;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.primary[800]};
+  }
+`;
