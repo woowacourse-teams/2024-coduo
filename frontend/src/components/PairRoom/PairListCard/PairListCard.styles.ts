cd707cc89c766a6a78d09aec0427b3a2a6f80493
greetings1012 <ahnqwerty@gmail.com>
2024-09-16 21:07:07 +0900

    ♻️ 버튼 클릭으로 사이드바 여닫도록 변경

--- frontend/src/components/PairRoom/PairListCard/PairListCard.styles.ts
+++ frontend/src/components/PairRoom/PairListCard/PairListCard.styles.ts
@@ -8,11 +8,11 @@ export const Layout = styled.div<{ $isOpen: boolean }>`
 
   position: relative;
 
-  min-width: ${(props) => (props.$isOpen ? '26rem' : '6rem')};
+  min-width: ${(props) => (props.$isOpen ? '24rem' : '6rem')};
 
   white-space: nowrap;
 
-  transition: all 0.3s;
+  transition: width 0.3s;
 `;
 
 export const Sidebar = styled.div`
