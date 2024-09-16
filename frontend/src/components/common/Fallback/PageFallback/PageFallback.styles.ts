34320d43b8d3852105a592f9eb8893e1a20f0427
greetings1012 <ahnqwerty@gmail.com>
2024-09-17 02:45:20 +0900

    💄 Lint:css:fix 적용

--- frontend/src/components/common/Fallback/PageFallback/PageFallback.styles.ts
+++ frontend/src/components/common/Fallback/PageFallback/PageFallback.styles.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 
   height: calc(100vh - 7rem);
   padding: 20px;
