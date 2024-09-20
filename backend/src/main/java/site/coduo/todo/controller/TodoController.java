800337e884dbaadd1953e3273c122c52d6cc3741
lemone <lejinhy@gmail.com>
2024-09-20 19:58:23 +0900

    fix: @PathVariable에 name 추가

--- backend/src/main/java/site/coduo/todo/controller/TodoController.java
+++ backend/src/main/java/site/coduo/todo/controller/TodoController.java
@@ -29,7 +29,7 @@ public class TodoController implements TodoDocs {
     private final TodoService todoService;
 
     @GetMapping("/{accessCode}/todos")
-    public List<GetTodoResponse> getTodos(@PathVariable final String accessCode) {
+    public List<GetTodoResponse> getTodos(@PathVariable("accessCode") final String accessCode) {
         final List<Todo> allTodos = todoService.getAllOrderBySort(accessCode);
         return IntStream.range(0, allTodos.size())
                 .mapToObj(index -> GetTodoResponse.from(allTodos.get(index), index))
@@ -37,28 +37,35 @@ public class TodoController implements TodoDocs {
     }
 
     @PostMapping("/{accessCode}/todos")
-    public void createTodo(@PathVariable final String accessCode, @RequestBody @Valid final CreateTodoRequest request) {
+    public void createTodo(
+            @PathVariable("accessCode") final String accessCode,
+            @RequestBody @Valid final CreateTodoRequest request
+    ) {
         todoService.createTodo(accessCode, request.content());
     }
 
     @PatchMapping("/todos/{todoId}/contents")
-    public void updateContent(@PathVariable final long todoId,
-                              @RequestBody @Valid final UpdateTodoContentRequest request) {
+    public void updateContent(
+            @PathVariable("todoId") final long todoId,
+            @RequestBody @Valid final UpdateTodoContentRequest request) {
         todoService.updateTodoContent(todoId, request.content());
     }
 
     @PatchMapping("/todos/{todoId}/checked")
-    public void toggleTodoChecked(@PathVariable final long todoId) {
+    public void toggleTodoChecked(@PathVariable("todoId") final long todoId) {
         todoService.toggleTodoChecked(todoId);
     }
 
     @PatchMapping("/todos/{todoId}/order")
-    public void updateTodoOrder(@PathVariable final long todoId, @RequestBody UpdateTodoOrderRequest request) {
+    public void updateTodoOrder(
+            @PathVariable("todoId") final long todoId,
+            @RequestBody UpdateTodoOrderRequest request
+    ) {
         todoService.updateTodoSort(todoId, request.order());
     }
 
     @DeleteMapping("/todos/{todoId}")
-    public void deleteTodo(@PathVariable final long todoId) {
+    public void deleteTodo(@PathVariable("todoId") final long todoId) {
         todoService.deleteTodo(todoId);
     }
 }
