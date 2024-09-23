5381ccd9f28a0fcacdd8eb662189613472a7d2e4
reddevilmidzy <midzy0228@gmail.com>
2024-09-23 13:28:52 +0900

    style: final 누락 추가

--- backend/src/main/java/site/coduo/pairroommember/controller/PairRoomMemberController.java
+++ backend/src/main/java/site/coduo/pairroommember/controller/PairRoomMemberController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import site.coduo.pairroommember.service.dto.PairRoomMemberResponse;
 import site.coduo.pairroommember.service.PairRoomMemberService;
+import site.coduo.pairroommember.service.dto.PairRoomMemberResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +20,9 @@ public class PairRoomMemberController {
     private final PairRoomMemberService pairRoomMemberService;
 
     @GetMapping("/my-pair-rooms")
-    public ResponseEntity<List<PairRoomMemberResponse>> getPairRooms(@CookieValue(SIGN_IN_COOKIE_NAME) String token) {
+    public ResponseEntity<List<PairRoomMemberResponse>> getPairRooms(
+            @CookieValue(SIGN_IN_COOKIE_NAME) final String token
+    ) {
         final List<PairRoomMemberResponse> pairRooms = pairRoomMemberService.findPairRooms(token);
 
         return ResponseEntity.ok()
