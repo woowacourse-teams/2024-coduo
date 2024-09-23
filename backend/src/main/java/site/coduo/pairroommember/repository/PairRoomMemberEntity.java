30b5ca4c931a5628445e02c78ec0706524a6aa61
reddevilmidzy <midzy0228@gmail.com>
2024-09-23 13:35:58 +0900

    refactor: 필드의 컬럼명 명시

--- backend/src/main/java/site/coduo/pairroommember/repository/PairRoomMemberEntity.java
+++ backend/src/main/java/site/coduo/pairroommember/repository/PairRoomMemberEntity.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -26,9 +27,11 @@ public class PairRoomMemberEntity {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "PAIR_ROOM_ID")
     private PairRoomEntity pairRoom;
 
     @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
     public PairRoomMemberEntity(final PairRoomEntity pairRoom, final Member member) {
