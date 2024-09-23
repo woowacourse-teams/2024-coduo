c534c563c5c3e8b229389ac67497667b358cde90
anttiey <hanuuny@gmail.com>
2024-09-23 18:14:04 +0900

    :lipstick: 마이 페이지 페어룸 상태별 스타일 분기 처리

--- frontend/src/pages/MyPage/MyPage.styles.ts
+++ frontend/src/pages/MyPage/MyPage.styles.ts
@@ -1,20 +1,17 @@
-import styled, { keyframes } from 'styled-components';
-
-const flow = keyframes`
-  0% {
-    background-position: 200% 0;
-  }
-  100% {
-    background-position: -200% 0;
-  }
-`;
+import styled from 'styled-components';
 
 export const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 5rem;
 
-  padding: 5rem 26vw;
+  width: 70rem;
+  margin: 5rem 10rem;
 
   h2 {
     font-size: ${({ theme }) => theme.fontSize.lg};
@@ -61,96 +58,11 @@ export const List = styled.div`
   gap: 1.6rem;
 `;
 
-export const PairRoomButton = styled.button`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  overflow: hidden;
-
-  position: relative;
-
-  width: 100%;
-  padding: 3rem;
-  border-radius: 1rem;
-
-  font-size: ${({ theme }) => theme.fontSize.base};
-
-  &::before {
-    content: '';
-
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-
-    width: 100%;
-    height: 100%;
-    border-radius: 1rem;
-
-    background: linear-gradient(
-      120deg,
-      ${({ theme }) => theme.color.secondary[100]} 0 75%,
-      ${({ theme }) => theme.color.secondary[600]} 75% 100%
-    );
-    opacity: 0.7;
-
-    transition: opacity 0.2s ease-in-out;
-  }
-
-  &:hover::before {
-    opacity: 1;
-  }
-`;
-
-export const PairRoleTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
 export const AllText = styled.p`
   padding-bottom: 1.6rem;
 
   color: ${({ theme }) => theme.color.black[70]};
   font-size: ${({ theme }) => theme.fontSize.md};
-`;
-
-export const PairRoleText = styled.p`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-
-  font-size: ${({ theme }) => theme.fontSize.md};
-
-  span {
-    color: ${({ theme }) => theme.color.secondary[600]};
-    font-size: ${({ theme }) => theme.fontSize.lg};
-    font-weight: ${({ theme }) => theme.fontWeight.medium};
-  }
-`;
-
-export const StatusText = styled.p`
-  background: linear-gradient(
-    90deg,
-    ${({ theme }) => theme.color.black[60]},
-    ${({ theme }) => theme.color.black[70]},
-    ${({ theme }) => theme.color.black[60]}
-  );
-  color: transparent;
-  font-size: ${({ theme }) => theme.fontSize.base};
-  letter-spacing: 0.15rem;
-
-  animation: ${flow} 4s linear infinite;
-  background-size: 200% 100%;
-  background-clip: text;
-`;
-
-export const ConnectText = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 0.4rem;
-
-  color: ${({ theme }) => theme.color.black[10]};
 `;
 
 export const LeaveButton = styled.button`
@@ -159,7 +71,7 @@ export const LeaveButton = styled.button`
   align-items: center;
   gap: 0.4rem;
 
-  color: ${({ theme }) => theme.color.black[50]};
+  color: ${({ theme }) => theme.color.black[60]};
   font-size: ${({ theme }) => theme.fontSize.md};
 
   transition: all 0.2s ease;
@@ -167,7 +79,7 @@ export const LeaveButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    color: ${({ theme }) => theme.color.black[60]};
+    color: ${({ theme }) => theme.color.black[70]};
     font-size: ${({ theme }) => theme.fontSize.md};
   }
 `;
--- frontend/src/pages/MyPage/MyPage.tsx
+++ frontend/src/pages/MyPage/MyPage.tsx
@@ -1,43 +1,34 @@
 import { IoIosArrowForward } from 'react-icons/io';
 
+import PairRoomButton from '@/components/MyPage/PairRoomButton/PairRoomButton';
+
 import * as S from './MyPage.styles';
 
 const MyPage = () => {
   return (
     <S.Layout>
-      <S.TitleContainer>
-        <S.Title>마이 페이지</S.Title>
-        <S.SubTitle>
-          <span>코듀오</span> 님의 마이 페이지에 오신 걸 환영합니다!
-        </S.SubTitle>
-      </S.TitleContainer>
-      <S.ListWrapper>
-        <h2>나의 페어룸 목록</h2>
-        <div>
-          <S.AllText>총 0개</S.AllText>
-          <S.List>
-            <S.PairRoomButton>
-              <S.PairRoleTextContainer>
-                <S.PairRoleText>
-                  <span>드라이버</span>레모네
-                </S.PairRoleText>
-                <S.PairRoleText>
-                  <span>내비게이터</span>파슬리
-                </S.PairRoleText>
-              </S.PairRoleTextContainer>
-              <S.StatusText>진행 중</S.StatusText>
-              <S.ConnectText>
-                접속
-                <IoIosArrowForward size="1.8rem" />
-              </S.ConnectText>
-            </S.PairRoomButton>
-          </S.List>
-        </div>
-      </S.ListWrapper>
-      <S.LeaveButton>
-        회원 탈퇴하기
-        <IoIosArrowForward size="1.5rem" />
-      </S.LeaveButton>
+      <S.Container>
+        <S.TitleContainer>
+          <S.Title>마이 페이지</S.Title>
+          <S.SubTitle>
+            <span>코듀오</span> 님의 마이 페이지에 오신 걸 환영합니다!
+          </S.SubTitle>
+        </S.TitleContainer>
+        <S.ListWrapper>
+          <h2>나의 페어룸 목록</h2>
+          <div>
+            <S.AllText>총 0개</S.AllText>
+            <S.List>
+              <PairRoomButton driver="레모네" navigator="파슬리" status="IN_PROGRESS" />
+              <PairRoomButton driver="레모네" navigator="파슬리" status="COMPLETED" />
+            </S.List>
+          </div>
+        </S.ListWrapper>
+        <S.LeaveButton>
+          회원 탈퇴하기
+          <IoIosArrowForward size="1.5rem" />
+        </S.LeaveButton>
+      </S.Container>
     </S.Layout>
   );
 };
