a91971ca8c5a567cd1d30b34c4e7f9286049efa3
kelly6bf <kelly6bf@gmail.com>
2024-09-23 14:29:05 +0900

    docs: #501 Todo 도메인 API 문서화

--- backend/src/main/java/site/coduo/todo/controller/TodoController.java
+++ backend/src/main/java/site/coduo/todo/controller/TodoController.java
@@ -1,10 +1,12 @@
 package site.coduo.todo.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import jakarta.validation.Valid;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -37,35 +39,40 @@ public class TodoController implements TodoDocs {
     }
 
     @PostMapping("/{accessCode}/todos")
-    public void createTodo(
+    public ResponseEntity<Void> createTodo(
             @PathVariable("accessCode") final String accessCode,
             @RequestBody @Valid final CreateTodoRequest request
     ) {
         todoService.createTodo(accessCode, request.content());
+        return ResponseEntity.created(URI.create("/")).build();
     }
 
     @PatchMapping("/todos/{todoId}/contents")
-    public void updateContent(
+    public ResponseEntity<Void> updateContent(
             @PathVariable("todoId") final long todoId,
             @RequestBody @Valid final UpdateTodoContentRequest request) {
         todoService.updateTodoContent(todoId, request.content());
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/todos/{todoId}/checked")
-    public void toggleTodoChecked(@PathVariable("todoId") final long todoId) {
+    public ResponseEntity<Void> toggleTodoChecked(@PathVariable("todoId") final long todoId) {
         todoService.toggleTodoChecked(todoId);
+        return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/todos/{todoId}/order")
-    public void updateTodoOrder(
+    public ResponseEntity<Void> updateTodoOrder(
             @PathVariable("todoId") final long todoId,
             @RequestBody UpdateTodoOrderRequest request
     ) {
         todoService.updateTodoSort(todoId, request.order());
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/todos/{todoId}")
-    public void deleteTodo(@PathVariable("todoId") final long todoId) {
+    public ResponseEntity<Void> deleteTodo(@PathVariable("todoId") final long todoId) {
         todoService.deleteTodo(todoId);
+        return ResponseEntity.noContent().build();
     }
 }
