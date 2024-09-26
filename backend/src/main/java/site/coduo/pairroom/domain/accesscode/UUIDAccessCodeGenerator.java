4321009ee2141400527e8691b7e4c734f0f9abb4
김민종 <koust6u@pusan.ac.kr>
2024-09-27 08:51:35 +0900

    feat: 엑세스 코드 생성 전략 수정 (#670) (#671)

--- backend/src/main/java/site/coduo/pairroom/domain/accesscode/UUIDAccessCodeGenerator.java
+++ backend/src/main/java/site/coduo/pairroom/domain/accesscode/UUIDAccessCodeGenerator.java
@@ -2,12 +2,19 @@ package site.coduo.pairroom.domain.accesscode;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UUIDAccessCodeGenerator {
 
     private static final int ACCESS_CODE_LENGTH = 6;
 
-    public static String generate() {
-        return UUID.randomUUID()
+    @Value("${ec2.prefix}")
+    private String prefix;
+
+    public String generate() {
+        return prefix + UUID.randomUUID()
                 .toString()
                 .substring(0, ACCESS_CODE_LENGTH);
     }
