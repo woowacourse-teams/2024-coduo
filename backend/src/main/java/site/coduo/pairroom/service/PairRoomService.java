426910057bd81c5a63e442477735331150fd7517
김민종 <koust6u@pusan.ac.kr>
2024-09-27 09:11:30 +0900

    Be/dev (#673)
    * feat: 엑세스 코드 생성 전략 수정 (#670)
    
    * Be/feature/pray (#672)
    
    * feat: 엑세스 코드 생성 전략 수정
    
    * feat: 엑세스 코드 생성 전략 수정

--- backend/src/main/java/site/coduo/pairroom/service/PairRoomService.java
+++ backend/src/main/java/site/coduo/pairroom/service/PairRoomService.java
@@ -64,6 +64,7 @@ public class PairRoomService {
 
     private AccessCode generateAccessCode() {
         final String generatedAccessCode = uuidAccessCodeGenerator.generate();
+        log.info("ACCESS CODE : {}", generatedAccessCode);
         if (pairRoomRepository.existsByAccessCode(generatedAccessCode)) {
             return generateAccessCode();
         }
