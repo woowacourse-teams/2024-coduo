f97f2c850369b91d9e8421d31eaa84440c826c2d
yechop <channetattoo@gmail.com>
2024-10-04 14:18:37 +0900

    refactor: 페어룸으로 레퍼런스링크 찾는 메서드

--- backend/src/main/java/site/coduo/referencelink/repository/ReferenceLinkRepository.java
+++ backend/src/main/java/site/coduo/referencelink/repository/ReferenceLinkRepository.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import site.coduo.pairroom.repository.PairRoomEntity;
 import site.coduo.referencelink.exception.ReferenceLinkException;
 
 public interface ReferenceLinkRepository extends JpaRepository<ReferenceLinkEntity, Long> {
 
+    List<ReferenceLinkEntity> findByPairRoomEntity(PairRoomEntity pairRoomEntity);
+
     default ReferenceLinkEntity fetchById(long id) {
         return findById(id)
-                .orElseThrow(() -> new ReferenceLinkException("존재하지 않는 링크입니다.."));
+                .orElseThrow(() -> new ReferenceLinkException("존재하지 않는 링크입니다."));
     }
 }
