8496c24f19be3a4f27be10705897ac93768f6b85
anttiey <hanuuny@gmail.com>
2024-10-07 16:54:12 +0900

    :recycle: 일정 빈도 이상 타이머 에러 발생 시 에러 페이지로 이동하도록 변경

---
+++ frontend/src/pages/Error/Error.tsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+import Button from '@/components/common/Button/Button';
+
+import * as S from './Error.styles';
+
+const Error = () => {
+  return (
+    <S.Layout>
+      <S.TitleContainer>
+        <S.Title>404</S.Title>
+        <S.SubTitle>페이지를 불러오는 중 문제가 발생했습니다.</S.SubTitle>
+      </S.TitleContainer>
+      <Link to="/">
+        <Button css={S.buttonStyles} size="lg" aria-label="홈으로 이동">
+          홈으로 이동
+        </Button>
+      </Link>
+    </S.Layout>
+  );
+};
+
+export default Error;
