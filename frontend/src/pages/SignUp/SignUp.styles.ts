0eb5ccc99b2e5d3720f1f9ded24fef3e6061b66c
anttiey <hanuuny@gmail.com>
2024-10-08 15:51:00 +0900

    :recycle: 엔터 버튼으로 계정 만들기 제어할 수 있도록 변경

--- frontend/src/pages/SignUp/SignUp.styles.ts
+++ frontend/src/pages/SignUp/SignUp.styles.ts
@@ -1,13 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const buttonStyles = css`
+  font-size: ${({ theme }) => theme.fontSize.md};
+`;
 
 export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 2rem;
+  gap: 6rem;
 
   height: calc(100vh - 7rem);
-  padding: 20px;
+  padding: 15rem 5rem;
 
   background-color: ${({ theme }) => theme.color.black[20]};
 `;
@@ -15,28 +19,17 @@ export const Layout = styled.div`
 export const LogoIconWithTitle = styled.img`
   width: 30rem;
   max-width: 40rem;
-  margin: 5rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3.6rem;
 `;
 
 export const Title = styled.h1`
-  margin-bottom: 2rem;
-
   color: ${({ theme }) => theme.color.primary[800]};
   font-size: ${({ theme }) => theme.fontSize.h5};
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-`;
-
-export const InputWrapper = styled.div`
-  width: 100%;
-  max-width: 40rem;
-  margin-bottom: 2rem;
-`;
-
-export const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  width: 100%;
-  max-width: 40rem;
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
 `;
