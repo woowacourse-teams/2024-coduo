7504502c362097309c30c85b7c411bb8eb348b12
greetings1012 <ahnqwerty@gmail.com>
2024-10-11 16:08:49 +0900

    💄 페어룸 목록 버튼 스타일 수정

--- frontend/src/components/MyPage/PairRoomButton/PairRoomButton.styles.ts
+++ frontend/src/components/MyPage/PairRoomButton/PairRoomButton.styles.ts
@@ -1,3 +1,6 @@
+import { Link as RouterLink } from 'react-router-dom';
+
+import { FaTrashAlt } from 'react-icons/fa';
 import styled, { keyframes, css } from 'styled-components';
 
 import type { PairRoomStatus } from '@/apis/pairRoom';
@@ -11,72 +14,10 @@ const flow = keyframes`
   }
 `;
 
-export const Layout = styled.button<{ $status: PairRoomStatus }>`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  overflow: hidden;
-
-  position: relative;
-
-  width: 100%;
-  padding: 3rem;
-  border-radius: 1rem;
-
+const commonTextStyles = css`
   font-size: ${({ theme }) => theme.fontSize.base};
 
-  &::before {
-    content: '';
-
-    position: absolute;
-    top: 0;
-    left: 0;
-    z-index: -1;
-
-    width: 100%;
-    height: 100%;
-    border-radius: 1rem;
-
-    background: ${({ $status, theme }) =>
-      $status === 'IN_PROGRESS'
-        ? `linear-gradient(
-      120deg,
-      ${theme.color.secondary[100]} 0 75%,
-      ${theme.color.secondary[600]} 75% 100%
-    )`
-        : `linear-gradient(
-      120deg,
-      ${theme.color.black[30]} 0 75%,
-      ${theme.color.black[60]} 75% 100%
-    )`};
-    opacity: 0.7;
-
-    transition: opacity 0.2s ease-in-out;
-  }
-
-  &:hover::before {
-    opacity: 1;
-  }
-`;
-
-export const RoleTextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-`;
-
-export const RoleText = styled.p<{ $status: PairRoomStatus }>`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-
-  font-size: ${({ theme }) => theme.fontSize.md};
-
-  span {
-    color: ${({ $status, theme }) => ($status === 'IN_PROGRESS' ? theme.color.secondary[600] : theme.color.black[70])};
-    font-size: ${({ theme }) => theme.fontSize.lg};
-    font-weight: ${({ theme }) => theme.fontWeight.medium};
-  }
+  transition: color 0.7s ease;
 `;
 
 const inProgressText = css`
@@ -92,18 +33,160 @@ const inProgressText = css`
   background-clip: text;
 `;
 
+export const Layout = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 3rem;
+
+  width: 100%;
+`;
+
+export const Link = styled(RouterLink)`
+  width: 100%;
+`;
+
 export const StatusText = styled.p<{ $status: PairRoomStatus }>`
+  width: 15%;
+
+  ${commonTextStyles}
   color: ${({ $status, theme }) => ($status === 'IN_PROGRESS' ? 'transparent' : theme.color.black[70])};
-  font-size: ${({ theme }) => theme.fontSize.base};
   letter-spacing: 0.15rem;
+  text-align: left;
 
   ${({ $status }) => $status === 'IN_PROGRESS' && inProgressText}
+
+  &:hover {
+    color: white;
+  }
+`;
+
+export const RoleTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  width: 50%;
+`;
+
+export const RoleText = styled.p<{ $status: PairRoomStatus }>`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  font-size: ${({ theme }) => theme.fontSize.md};
+
+  transition: color 0.7s ease;
+
+  span {
+    color: ${({ $status, theme }) => ($status === 'IN_PROGRESS' ? theme.color.secondary[600] : theme.color.black[70])};
+    font-size: ${({ theme }) => theme.fontSize.lg};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+
+    transition: color 0.7s ease;
+  }
 `;
 
 export const ConnectText = styled.div`
   display: flex;
+  justify-content: right;
   align-items: center;
   gap: 0.4rem;
 
+  width: 10%;
+
   color: ${({ theme }) => theme.color.black[10]};
+
+  transition: color 0.7s ease;
+`;
+
+export const PairRoomButton = styled.button<{ $status: PairRoomStatus }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  overflow: hidden;
+
+  position: relative;
+
+  width: 100%;
+  padding: 3rem;
+  border-radius: 1rem;
+
+  font-size: ${({ theme }) => theme.fontSize.base};
+
+  cursor: pointer;
+
+  &::before {
+    content: '';
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
+
+    background-color: ${({ $status, theme }) =>
+      $status === 'IN_PROGRESS' ? theme.color.secondary[100] : theme.color.black[30]};
+    background-image: ${({ $status, theme }) =>
+      $status === 'IN_PROGRESS'
+        ? `linear-gradient(
+      90deg,
+      ${theme.color.secondary[100]} 0 75%,
+      ${theme.color.secondary[600]} 75% 100%
+    )`
+        : `linear-gradient(
+      90deg,
+      ${theme.color.black[30]} 0 75%,
+      ${theme.color.black[60]} 75% 100%
+    )`};
+    background-size: 400% 100%;
+
+    background-position: 72.5% 0;
+    opacity: 0.7;
+
+    transition:
+      background-position 0.5s ease,
+      opacity 0.2s ease;
+  }
+
+  &:hover::before {
+    background-position: 105.8% 0;
+    opacity: 1;
+  }
+
+  &:hover ${StatusText} {
+    color: white;
+    ${({ $status }) =>
+      $status === 'IN_PROGRESS' &&
+      css`
+        animation: ${flow} 4s linear infinite;
+      `}
+  }
+  &:hover ${RoleText} {
+    color: ${({ theme }) => theme.color.black[20]};
+
+    span {
+      color: ${({ theme }) => theme.color.black[10]};
+    }
+  }
+
+  &:hover ${ConnectText} {
+    color: ${({ theme }) => theme.color.black[70]};
+  }
+`;
+
+export const DeleteButton = styled(FaTrashAlt)`
+  color: ${({ theme }) => theme.color.black[60]};
+  font-size: 1.6rem;
+
+  transition: color 0.3s ease;
+
+  cursor: pointer;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.danger[600]};
+  }
 `;
--- frontend/src/components/MyPage/PairRoomButton/PairRoomButton.tsx
+++ frontend/src/components/MyPage/PairRoomButton/PairRoomButton.tsx
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import { IoIosArrowForward } from 'react-icons/io';
 
-import type { PairRoomStatus } from '@/apis/pairRoom';
+import PairRoomDeleteModal from '@/components/MyPage/PairRoomDeleteModal/PairRoomDeleteModal';
+
+import { deletePairRoom, type PairRoomStatus } from '@/apis/pairRoom';
 
 import * as S from './PairRoomButton.styles';
 
@@ -14,26 +16,47 @@ interface PairRoomButtonProps {
 }
 
 const PairRoomButton = ({ driver, navigator, status, accessCode }: PairRoomButtonProps) => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+  const handleOpenDeleteModal = (event: React.MouseEvent<HTMLButtonElement | SVGElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleDeletePairRoom = async () => {
+    await deletePairRoom(accessCode);
+    setIsDeleteModalOpen(false);
+  };
+
   return (
-    <Link to={`/room/${accessCode}`}>
-      <S.Layout $status={status}>
-        <S.RoleTextContainer>
-          <S.RoleText $status={status}>
-            <span>드라이버</span>
-            {driver}
-          </S.RoleText>
-          <S.RoleText $status={status}>
-            <span>내비게이터</span>
-            {navigator}
-          </S.RoleText>
-        </S.RoleTextContainer>
-        <S.StatusText $status={status}>{status === 'IN_PROGRESS' ? '진행 중' : '진행 완료'}</S.StatusText>
-        <S.ConnectText>
-          접속
-          <IoIosArrowForward size="1.8rem" />
-        </S.ConnectText>
-      </S.Layout>
-    </Link>
+    <S.Layout>
+      <S.Link to={`/room/${accessCode}`}>
+        <S.PairRoomButton $status={status}>
+          <S.RoleTextContainer>
+            <S.RoleText $status={status}>
+              <span>드라이버</span>
+              {driver}
+            </S.RoleText>
+            <S.RoleText $status={status}>
+              <span>내비게이터</span>
+              {navigator}
+            </S.RoleText>
+          </S.RoleTextContainer>
+          <S.StatusText $status={status}>{status === 'IN_PROGRESS' ? '진행 중' : '진행 완료'}</S.StatusText>
+          <S.ConnectText>
+            입장
+            <IoIosArrowForward size="1.8rem" />
+          </S.ConnectText>
+        </S.PairRoomButton>
+      </S.Link>
+      <S.DeleteButton onClick={handleOpenDeleteModal} />
+      <PairRoomDeleteModal
+        isOpen={isDeleteModalOpen}
+        closeModal={() => setIsDeleteModalOpen(false)}
+        handleDeletePairRoom={handleDeletePairRoom}
+      />
+    </S.Layout>
   );
 };
 
