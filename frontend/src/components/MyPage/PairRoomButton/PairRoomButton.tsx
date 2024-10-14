7b3ac6569168e2e0a8606957444cc65a58d33344
greetings1012 <ahnqwerty@gmail.com>
2024-10-14 18:51:19 +0900

    ♻️ Tanstack query, useModal 훅 사용

--- frontend/src/components/MyPage/PairRoomButton/PairRoomButton.tsx
+++ frontend/src/components/MyPage/PairRoomButton/PairRoomButton.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react';
-
 import { IoIosArrowForward } from 'react-icons/io';
 
+import Spinner from '@/components/common/Spinner/Spinner';
 import PairRoomDeleteModal from '@/components/MyPage/PairRoomDeleteModal/PairRoomDeleteModal';
 
-import { deletePairRoom, type PairRoomStatus } from '@/apis/pairRoom';
+import { type PairRoomStatus } from '@/apis/pairRoom';
+
+import useModal from '@/hooks/common/useModal';
+
+import useDeletePairRoom from '@/queries/MyPage/useDeleteRoom';
 
 import * as S from './PairRoomButton.styles';
 
@@ -16,46 +19,52 @@ interface PairRoomButtonProps {
 }
 
 const PairRoomButton = ({ driver, navigator, status, accessCode }: PairRoomButtonProps) => {
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-
+  const { openModal, closeModal, isModalOpen } = useModal();
+  const { mutate, isPending } = useDeletePairRoom();
   const handleOpenDeleteModal = (event: React.MouseEvent<HTMLButtonElement | SVGElement>) => {
     event.preventDefault();
     event.stopPropagation();
-    setIsDeleteModalOpen(true);
+    openModal();
   };
 
   const handleDeletePairRoom = async () => {
-    await deletePairRoom(accessCode);
-    setIsDeleteModalOpen(false);
+    mutate(accessCode);
+    closeModal();
   };
 
   return (
     <S.Layout>
-      <S.LinkWrapper to={`/room/${accessCode}`}>
-        <S.PairRoomButton $status={status}>
-          <S.RoleTextContainer>
-            <S.RoleText $status={status}>
-              <span>드라이버</span>
-              {driver}
-            </S.RoleText>
-            <S.RoleText $status={status}>
-              <span>내비게이터</span>
-              {navigator}
-            </S.RoleText>
-          </S.RoleTextContainer>
-          <S.StatusText $status={status}>{status === 'IN_PROGRESS' ? '진행 중' : '진행 완료'}</S.StatusText>
-          <S.ConnectText>
-            입장
-            <IoIosArrowForward size="1.8rem" />
-          </S.ConnectText>
-        </S.PairRoomButton>
-      </S.LinkWrapper>
-      <S.DeleteButton onClick={handleOpenDeleteModal} />
-      <PairRoomDeleteModal
-        isOpen={isDeleteModalOpen}
-        closeModal={() => setIsDeleteModalOpen(false)}
-        handleDeletePairRoom={handleDeletePairRoom}
-      />
+      {isPending ? (
+        <Spinner />
+      ) : (
+        <>
+          <S.LinkWrapper to={`/room/${accessCode}`}>
+            <S.PairRoomButton $status={status}>
+              <S.RoleTextContainer>
+                <S.RoleText $status={status}>
+                  <span>드라이버</span>
+                  {driver}
+                </S.RoleText>
+                <S.RoleText $status={status}>
+                  <span>내비게이터</span>
+                  {navigator}
+                </S.RoleText>
+              </S.RoleTextContainer>
+              <S.StatusText $status={status}>{status === 'IN_PROGRESS' ? '진행 중' : '진행 완료'}</S.StatusText>
+              <S.ConnectText>
+                입장
+                <IoIosArrowForward size="1.8rem" />
+              </S.ConnectText>
+            </S.PairRoomButton>
+          </S.LinkWrapper>
+          <S.DeleteButton onClick={handleOpenDeleteModal} />
+          <PairRoomDeleteModal
+            isOpen={isModalOpen}
+            closeModal={closeModal}
+            handleDeletePairRoom={handleDeletePairRoom}
+          />
+        </>
+      )}
     </S.Layout>
   );
 };
