6f3eae21e7d07eac42b1acd4ab6dfdae660d535e
greetings1012 <ahnqwerty@gmail.com>
2024-10-17 11:36:59 +0900

    💄 페어룸 완료 페이지 디자인 개선

--- frontend/src/pages/CompletedPairRoom/CompletedPairRoom.tsx
+++ frontend/src/pages/CompletedPairRoom/CompletedPairRoom.tsx
@@ -5,6 +5,7 @@ import { GithubLogoWhite } from '@/assets';
 
 import Loading from '@/pages/Loading/Loading';
 
+import { ScrollAnimationContainer } from '@/components/common/Animation/ScrollAnimationContainer';
 import Button from '@/components/common/Button/Button';
 import ReferenceCard from '@/components/CompletedPairRoom/ReferenceCard/ReferenceCard';
 import TodoListCard from '@/components/CompletedPairRoom/TodoListCard/TodoListCard';
@@ -47,37 +48,49 @@ const CompletedPairRoom = () => {
   return (
     <S.Layout>
       <S.CompletedPairRoomInformationContainer>
-        <S.Title>{accessCode}</S.Title>
-        <S.PairInfo>
-          <S.FirstPair>{driver}</S.FirstPair>과&nbsp;
-          <S.SecondPair>{navigator}</S.SecondPair>이 함께했던 페어룸이에요.
-        </S.PairInfo>
-        {missionUrl && (
-          <S.RepositoryLink href={missionUrl} target="_blank">
-            <S.GithubLogo src={GithubLogoWhite} />
-            미션 리포지토리로 이동
-          </S.RepositoryLink>
-        )}
-        {userStatus === 'SIGNED_IN' ? ( // TODO: 회고 작성 여부 확인 로직 추가
-          <Button
-            size="lg"
-            onClick={() => {
-              handleReviewWriting();
-            }}
-          >
-            회고 작성
-          </Button>
-        ) : (
-          <S.LoginPromptContainer>
-            <Button size="lg" disabled={true}>
+        <ScrollAnimationContainer animationDirection="right">
+          <S.Title>{accessCode}</S.Title>
+        </ScrollAnimationContainer>
+        <ScrollAnimationContainer animationDirection="right" animationDelay={0.2}>
+          <S.PairInfo>
+            <S.FirstPair>{driver}</S.FirstPair>와(과)&nbsp;
+            <S.SecondPair>{navigator}</S.SecondPair>이(가) 함께했던 페어룸이에요.
+          </S.PairInfo>
+        </ScrollAnimationContainer>
+        <ScrollAnimationContainer animationDirection="right" animationDelay={0.4}>
+          {missionUrl && (
+            <S.RepositoryLink href={missionUrl} target="_blank">
+              <S.GithubLogo src={GithubLogoWhite} />
+              미션 리포지토리로 이동
+            </S.RepositoryLink>
+          )}
+        </ScrollAnimationContainer>
+        <ScrollAnimationContainer animationDirection="right" animationDelay={0.4}>
+          {userStatus === 'SIGNED_IN' ? ( // TODO: 회고 작성 여부 확인 로직 추가
+            <Button
+              size="lg"
+              onClick={() => {
+                handleReviewWriting();
+              }}
+            >
               회고 작성
             </Button>
-            <S.LoginPrompt>로그인하면 회고를 작성할 수 있어요.</S.LoginPrompt>
-          </S.LoginPromptContainer>
-        )}
+          ) : (
+            <S.LoginPromptContainer>
+              <Button size="lg" disabled={true}>
+                회고 작성
+              </Button>
+              <S.LoginPrompt>로그인 후 페어룸에 참여하면 회고를 작성할 수 있어요.</S.LoginPrompt>
+            </S.LoginPromptContainer>
+          )}
+        </ScrollAnimationContainer>
       </S.CompletedPairRoomInformationContainer>
-      <TodoListCard />
-      <ReferenceCard accessCode={accessCode || ''} />
+      <ScrollAnimationContainer animationDirection="top" animationDelay={0.4}>
+        <TodoListCard />
+      </ScrollAnimationContainer>
+      <ScrollAnimationContainer animationDirection="top" animationDelay={0.6}>
+        <ReferenceCard accessCode={accessCode || ''} />
+      </ScrollAnimationContainer>
     </S.Layout>
   );
 };
