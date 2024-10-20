34b7943a66080e5937eb9a1bfdef717112bcc022
lemone <lejinhy@gmail.com>
2024-10-20 19:18:02 +0900

    feat: 카테고리 & 레퍼런스 링크 스크립트 추가

---
+++ backend/k6/reference-link.js
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+import {check} from 'k6';
+import {BASE_URL} from "./config.js";
+
+export function createReferenceLink(accessCode) {
+    const url = `${BASE_URL}${accessCode}/reference-link`;
+    const payload = JSON.stringify({
+        "url": "https://test.coduo.site"
+    });
+    const param = {
+        headers: {
+            'Content-Type' : 'application/json',
+        },
+    }
+
+    const res = http.post(url, payload, param);
+    const body = JSON.parse(res.body);
+
+    check(res, {
+        'Create reference link status was 201': (r) => r.status === 201,
+    });
+
+    getReferenceLink(body.accessCode);
+    deleteReferenceLink(body.accessCode, 1);
+}
+
+export function getReferenceLink(accessCode) {
+    const url = `${BASE_URL}${accessCode}/reference-link`;
+    let res = http.get(url);
+
+    check(res, {
+        'Get reference link was 200': (r) => r.status === 200,
+    });
+}
+
+export function deleteReferenceLink(accessCode, id) {
+    const url = `${BASE_URL}${accessCode}/reference-link/${id}`;
+    let res = http.del(url);
+    check(res, {
+        'Delete reference link status was 204': (r) => r.status === 204,
+    });
+}
