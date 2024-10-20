844a850fa7bef6204a492e266265deff71082015
anttiey <hanuuny@gmail.com>
2024-10-20 15:59:23 +0900

    :recycle: 페어 연동 버튼 추가

--- frontend/src/components/PairRoomOnboarding/PairNameInput/PairNameInput.styles.ts
+++ frontend/src/components/PairRoomOnboarding/PairNameInput/PairNameInput.styles.ts
@@ -1,4 +1,18 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+export const buttonStyles = css`
+  width: 8rem;
+  height: 4rem;
+
+  font-size: ${({ theme }) => theme.fontSize.md};
+`;
+
+export const cancelButtonStyles = css`
+  ${buttonStyles};
+  border-color: ${({ theme }) => theme.color.black[50]};
+
+  background-color: ${({ theme }) => theme.color.black[50]};
+`;
 
 export const Layout = styled.div`
   display: flex;
@@ -28,4 +42,86 @@ export const InputContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1rem;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const Label = styled.p`
+  color: ${({ theme }) => theme.color.primary[700]};
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+`;
+
+export const AddButton = styled.button`
+  display: flex;
+  align-items: center;
+
+  font-size: ${({ theme }) => theme.fontSize.md};
+`;
+
+export const Logo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 4.2rem;
+  height: 4rem;
+  border-radius: 0.5rem 0 0 0.5rem;
+
+  background-color: ${({ theme }) => theme.color.primary[800]};
+
+  img {
+    width: 2.2rem;
+    height: 2.2rem;
+  }
+`;
+
+export const AddText = styled.p`
+  display: flex;
+  align-items: center;
+
+  height: 4rem;
+  padding: 1rem 2rem;
+  border-radius: 0 0.5rem 0.5rem 0;
+
+  background-color: ${({ theme }) => theme.color.primary[500]};
+  color: ${({ theme }) => theme.color.black[10]};
+`;
+
+export const TextButton = styled.button`
+  color: ${({ theme }) => theme.color.black[60]};
+  font-size: ${({ theme }) => theme.fontSize.sm};
+  text-decoration: underline;
+
+  transition: all 0.2s;
+
+  &:hover {
+    color: ${({ theme }) => theme.color.black[65]};
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+`;
+
+export const NameText = styled.p`
+  font-size: ${({ theme }) => theme.fontSize.base};
 `;
--- frontend/src/components/PairRoomOnboarding/PairNameInput/PairNameInput.tsx
+++ frontend/src/components/PairRoomOnboarding/PairNameInput/PairNameInput.tsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
+
+import { LuPencil } from 'react-icons/lu';
+
+import { LogoIcon } from '@/assets';
+
+import Button from '@/components/common/Button/Button';
 import Input from '@/components/common/Input/Input';
 import { InputType } from '@/components/common/Input/Input.type';
+
+import useUserStore from '@/stores/userStore';
 
 import * as S from './PairNameInput.styles';
 
@@ -11,6 +20,11 @@ interface PairNameInputProps {
 }
 
 const PairNameInput = ({ firstPairName, secondPairName, onFirstPair, onSecondPair }: PairNameInputProps) => {
+  const { username, userStatus } = useUserStore();
+
+  const [isMyInputOpen, setIsMyInputOpen] = useState(false);
+  const [isPairInputOpen, setIsPairInputOpen] = useState(false);
+
   return (
     <S.Layout>
       <S.TitleContainer>
@@ -18,24 +32,59 @@ const PairNameInput = ({ firstPairName, secondPairName, onFirstPair, onSecondPai
         <S.SubTitle>나와 페어의 이름을 입력해 주세요.</S.SubTitle>
       </S.TitleContainer>
       <S.InputContainer>
-        <Input
-          placeholder="이름을 입력해주세요"
-          label="나의 이름은 무엇인가요?"
-          value={firstPairName.value}
-          status={firstPairName.status}
-          message={firstPairName.message}
-          onChange={onFirstPair}
-        />
+        <S.Label>나의 이름은 무엇인가요?</S.Label>
+        {userStatus === 'SIGNED_IN' && !isMyInputOpen ? (
+          <S.TextWrapper>
+            <S.NameText>{username}</S.NameText>
+            <LuPencil size="1.4rem" onClick={() => setIsMyInputOpen(true)} />
+          </S.TextWrapper>
+        ) : (
+          <S.InputWrapper>
+            <Input
+              placeholder="이름을 입력해주세요"
+              value={userStatus === 'SIGNED_IN' ? username : firstPairName.value}
+              status={firstPairName.status}
+              message={firstPairName.message}
+              onChange={onFirstPair}
+            />
+            <S.ButtonContainer>
+              <Button css={S.cancelButtonStyles} onClick={() => setIsMyInputOpen(false)}>
+                취소
+              </Button>
+              <Button css={S.buttonStyles}>확인</Button>
+            </S.ButtonContainer>
+          </S.InputWrapper>
+        )}
       </S.InputContainer>
       <S.InputContainer>
-        <Input
-          placeholder="이름을 입력해주세요"
-          label="함께할 페어의 이름은 무엇인가요?"
-          value={secondPairName.value}
-          status={secondPairName.status}
-          message={secondPairName.message}
-          onChange={onSecondPair}
-        />
+        <S.Label>함께할 페어의 이름은 무엇인가요?</S.Label>
+        {isPairInputOpen ? (
+          <S.InputWrapper>
+            <Input
+              placeholder="이름을 입력해주세요"
+              value={secondPairName.value}
+              status={secondPairName.status}
+              message={secondPairName.message}
+              onChange={onSecondPair}
+            />
+            <S.ButtonContainer>
+              <Button css={S.cancelButtonStyles} onClick={() => setIsPairInputOpen(false)}>
+                취소
+              </Button>
+              <Button css={S.buttonStyles}>확인</Button>
+            </S.ButtonContainer>
+          </S.InputWrapper>
+        ) : (
+          <S.ButtonWrapper>
+            <S.AddButton>
+              <S.Logo>
+                <img src={LogoIcon} alt="" />
+              </S.Logo>
+              <S.AddText>페어 정보 연동하기</S.AddText>
+            </S.AddButton>
+            <S.TextButton onClick={() => setIsPairInputOpen(true)}>연동 없이 시작하기</S.TextButton>
+          </S.ButtonWrapper>
+        )}
       </S.InputContainer>
     </S.Layout>
   );
