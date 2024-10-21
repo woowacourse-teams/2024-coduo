bf4a636f9fe29a6ae8625d52076913adef1738f1
reddevilmidzy <midzy0228@gmail.com>
2024-10-21 14:37:54 +0900

    style: 공백 제거

--- backend/src/main/java/site/coduo/pairroom/service/dto/PairUpdateRequest.java
+++ backend/src/main/java/site/coduo/pairroom/service/dto/PairUpdateRequest.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "페어 추가 요청 바디")
 public record PairUpdateRequest(
-
         @Schema(description = "현재 페어룸 접근 코드", example = "IAM0000000-")
         @NotBlank
         String accessCode,
