58424ff30b5787b09f99e47c9a9b2763dac2b3ff
lemone <lejinhy@gmail.com>
2024-10-21 16:48:37 +0900

    chore: base url 테스트 서버로 변경

--- backend/k6/config.js
+++ backend/k6/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://api.coduo.site/api/';
+export const BASE_URL = 'https://api-test.coduo.site/api/';
 export const CODUO_WHO_AM_I = 'eyJhbGciOiJIUzUxMiJ9.eyJpYXQiOjE3Mjk0MTIzNzEsInN1YiI6IjE3NzU5NTkyMyJ9.G_4JrUINNKMsYh0NhFMxKnhXTU9e9DwkN5vMejo2Qrlg4qL_M0mWil3q5DXzhOPUi2eotLD93JYCnKL7-KluVg';
