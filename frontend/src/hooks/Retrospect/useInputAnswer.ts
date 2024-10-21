7247f6bca08c6d2c5a34d4904bdf3b6bab46bcac
greetings1012 <ahnqwerty@gmail.com>
2024-10-21 21:24:51 +0900

    🐛 잘못된 api body id값 수정

--- frontend/src/hooks/Retrospect/useInputAnswer.ts
+++ frontend/src/hooks/Retrospect/useInputAnswer.ts
@@ -6,26 +6,26 @@ import { useAddRetrospect } from '@/queries/Retrospect/useAddRetrospect';
 import { RETROSPECT_QUESTIONS } from '@/constants/retrospect';
 
 const useInputAnswer = (pairRoomAccessCode: string) => {
-  const [answers, setAnswers] = useState<string[]>(Array(RETROSPECT_QUESTIONS.length).fill(''));
+  const [answer, setAnswer] = useState<string[]>(Array(RETROSPECT_QUESTIONS.length).fill(''));
   const { mutateAsync } = useAddRetrospect();
   const navigate = useNavigate();
   const handleChange = (index: number, value: string) => {
-    const newAnswers = [...answers];
-    newAnswers[index] = value;
-    setAnswers(newAnswers);
+    const newAnswer = [...answer];
+    newAnswer[index] = value;
+    setAnswer(newAnswer);
   };
 
   const hasEmptyField = () => {
-    return answers.some((item) => item.replace(/\s/g, '') === '');
+    return answer.some((item) => item.replace(/\s/g, '') === '');
   };
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
 
-    await mutateAsync({ pairRoomAccessCode, answers }).then(() => navigate(`/room/${pairRoomAccessCode}/completed`));
+    await mutateAsync({ pairRoomAccessCode, answer }).then(() => navigate(`/room/${pairRoomAccessCode}/completed`));
   };
 
-  return { answers, handleChange, hasEmptyField, handleSubmit };
+  return { answer, handleChange, hasEmptyField, handleSubmit };
 };
 
 export default useInputAnswer;
