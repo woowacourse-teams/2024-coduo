f6817ba19ee0df22730c0e5395fb7f92726c5b47
anttiey <hanuuny@gmail.com>
2024-10-22 15:34:51 +0900

    :wheelchair: 페어 역할 aria-label 추가

--- frontend/src/components/PairRoom/PairRoleCard/PairRoleCard.tsx
+++ frontend/src/components/PairRoom/PairRoleCard/PairRoleCard.tsx
@@ -10,24 +10,23 @@ interface PairRoleCardProps {
 
 const PairRoleCard = ({ driver, navigator }: PairRoleCardProps) => {
   return (
-    <S.Layout>
+    <S.Layout aria-label="페어 역할">
       <PairRoomCard>
         <S.RoleBoxContainer>
           <S.DriverBox>
-            <S.RoleIcon>💻</S.RoleIcon>
-            <S.RoleTextContainer>
+            <S.RoleIcon aria-hidden={true}>💻</S.RoleIcon>
+            <S.RoleTextContainer aria-label={`현재 드라이버는 ${driver}입니다.`}>
               <Tooltip
                 direction="top"
                 message="드라이버는 내비게이터가 설명한 방식대로 실제 코드를 작성하는 역할을 합니다."
               >
                 <S.DriverLabel>드라이버</S.DriverLabel>
               </Tooltip>
-
               <S.DriverText>{driver}</S.DriverText>
             </S.RoleTextContainer>
           </S.DriverBox>
           <S.NavigatorBox>
-            <S.RoleTextContainer>
+            <S.RoleTextContainer aria-label={`현재 내비게이터는 ${navigator}입니다.`}>
               <Tooltip
                 direction="top"
                 message="내비게이터는 코드의 논리적 흐름, 설계, 오류 등을 검토하며, 드라이버에게 피드백을 제공합니다."
@@ -36,7 +35,7 @@ const PairRoleCard = ({ driver, navigator }: PairRoleCardProps) => {
               </Tooltip>
               <S.NavigatorText>{navigator}</S.NavigatorText>
             </S.RoleTextContainer>
-            <S.RoleIcon>🧭</S.RoleIcon>
+            <S.RoleIcon aria-hidden={true}>🧭</S.RoleIcon>
           </S.NavigatorBox>
         </S.RoleBoxContainer>
       </PairRoomCard>
