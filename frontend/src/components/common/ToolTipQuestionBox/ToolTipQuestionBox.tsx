2012d26ce77d25bb8b16116c3dc40bb21c2ea4bd
greetings1012 <ahnqwerty@gmail.com>
2024-10-23 14:33:27 +0900

    ♿️ 스크린 리더가 툴팁 물음표 상자의 내용도 읽도록 수정

--- frontend/src/components/common/ToolTipQuestionBox/ToolTipQuestionBox.tsx
+++ frontend/src/components/common/ToolTipQuestionBox/ToolTipQuestionBox.tsx
@@ -20,7 +20,11 @@ const ToolTipQuestionBox = ({
 }: ToolTipQuestionBoxProps) => {
   return (
     <Tooltip direction={boxDirection} color={boxColor} {...props}>
-      <S.QuestionIcon $color={color} />
+      <S.QuestionIcon
+        $color={color}
+        aria-roledescription="tooltip"
+        aria-label="페어 프로그래밍을 위해 필요한 할 일 목록을 작성해 보세요. 할 일을 더욱 효율적으로 관리할 수 있습니다."
+      />
     </Tooltip>
   );
 };
