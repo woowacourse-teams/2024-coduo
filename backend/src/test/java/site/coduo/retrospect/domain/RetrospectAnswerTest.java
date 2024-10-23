54cebcd42d6a8db9cf0dbfdc9db80cf5c8e5ab43
reddevilmidzy <midzy0228@gmail.com>
2024-10-23 10:58:04 +0900

    fix: 회고 컨텐츠 null 허용

--- backend/src/test/java/site/coduo/retrospect/domain/RetrospectAnswerTest.java
+++ backend/src/test/java/site/coduo/retrospect/domain/RetrospectAnswerTest.java
@@ -5,8 +5,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import site.coduo.retrospect.exception.InvalidRetrospectContentException;
 
@@ -23,16 +21,6 @@ class RetrospectAnswerTest {
 
         // Then
         assertThat(retrospectAnswer).isNotNull();
-    }
-
-    @DisplayName("유효하지 않은 회고 답변 값이 입력되면 예외를 발생시킨다.")
-    @NullAndEmptySource
-    @ParameterizedTest
-    void validateValueIsNull(final String input) {
-        // When & Then
-        assertThatThrownBy(() -> new RetrospectAnswer(input))
-                .isInstanceOf(InvalidRetrospectContentException.class)
-                .hasMessage("회고 답변 내용으로 null 혹은 공백을 입력할 수 없습니다. - " + input);
     }
 
     @DisplayName("유효하지 않은 길이의 회고 답변이 입력되면 예외를 발생시킨다.")
