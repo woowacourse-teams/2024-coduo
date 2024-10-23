41cdffd8c91b3b4875d41878f5b1bef100ce7a50
anttiey <hanuuny@gmail.com>
2024-10-24 01:02:19 +0900

    :recycle: 회고 버튼 책임 분리 및 애니메이션 단순화

--- frontend/src/pages/CompletedPairRoom/CompletedPairRoom.tsx
+++ frontend/src/pages/CompletedPairRoom/CompletedPairRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 import { GithubLogoWhite } from '@/assets';
 
@@ -7,10 +7,7 @@ import Loading from '@/pages/Loading/Loading';
 import { ScrollAnimationContainer } from '@/components/common/Animation/ScrollAnimationContainer';
 import ReferenceCard from '@/components/CompletedPairRoom/ReferenceCard/ReferenceCard';
 import RetrospectButton from '@/components/CompletedPairRoom/RetrospectButton/RetrospectButton';
-import RetrospectButtonDisabled from '@/components/CompletedPairRoom/RetrospectButton/RetroSpectButtonDisabled';
 import TodoListCard from '@/components/CompletedPairRoom/TodoListCard/TodoListCard';
-
-import useUserStore from '@/stores/userStore';
 
 import useGetPairRoom from '@/queries/PairRoom/useGetPairRoom';
 
@@ -19,7 +16,6 @@ import * as S from './CompletedPairRoom.styles';
 const CompletedPairRoom = () => {
   const { accessCode } = useParams();
 
-  const { userStatus } = useUserStore();
   const { driver, navigator, missionUrl, isFetching } = useGetPairRoom(accessCode || '');
 
   if (isFetching) {
@@ -28,38 +24,30 @@ const CompletedPairRoom = () => {
 
   return (
     <S.Layout>
-      <S.CompletedPairRoomInformationContainer>
-        <ScrollAnimationContainer animationDirection="right">
-          <S.Title>{accessCode}</S.Title>
-        </ScrollAnimationContainer>
-        <ScrollAnimationContainer animationDirection="right" animationDelay={0.2}>
-          <S.PairInfo>
-            <S.FirstPair>{driver}</S.FirstPair>와(과)&nbsp;
-            <S.SecondPair>{navigator}</S.SecondPair>의 기록이에요.
-          </S.PairInfo>
-        </ScrollAnimationContainer>
-        <ScrollAnimationContainer animationDirection="right" animationDelay={0.4}>
+      <ScrollAnimationContainer animationDirection="right">
+        <S.InfoContainer>
+          <S.TitleContainer>
+            <S.Title>{accessCode}</S.Title>
+            <S.PairInfoWrapper>
+              <S.FirstPair>{driver}</S.FirstPair>와(과)&nbsp;
+              <S.SecondPair>{navigator}</S.SecondPair>의 기록이에요
+            </S.PairInfoWrapper>
+          </S.TitleContainer>
           {missionUrl && (
-            <S.RepositoryLink href={missionUrl} target="_blank">
-              <S.GithubLogo src={GithubLogoWhite} />
-              미션 리포지토리 열기
-            </S.RepositoryLink>
+            <Link to={missionUrl} target="_blank">
+              <S.RepositoryButton>
+                <S.GithubLogo src={GithubLogoWhite} />
+                미션 리포지토리로 이동
+              </S.RepositoryButton>
+            </Link>
           )}
-        </ScrollAnimationContainer>
-        <ScrollAnimationContainer animationDirection="right" animationDelay={0.4}>
-          {userStatus === 'SIGNED_IN' ? (
-            <RetrospectButton accessCode={accessCode || ''} />
-          ) : (
-            <RetrospectButtonDisabled />
-          )}
-        </ScrollAnimationContainer>
-      </S.CompletedPairRoomInformationContainer>
-      <ScrollAnimationContainer animationDirection="top" animationDelay={0.4}>
+          <RetrospectButton accessCode={accessCode || ''} />
+        </S.InfoContainer>
+      </ScrollAnimationContainer>
+      <S.CardContainer>
         <TodoListCard />
-      </ScrollAnimationContainer>
-      <ScrollAnimationContainer animationDirection="top" animationDelay={0.6}>
         <ReferenceCard accessCode={accessCode || ''} />
-      </ScrollAnimationContainer>
+      </S.CardContainer>
     </S.Layout>
   );
 };
