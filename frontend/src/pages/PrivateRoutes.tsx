662a2c5326cbea4c729ab53af9a8ac0967eab5da
anttiey <hanuuny@gmail.com>
2024-10-24 21:18:09 +0900

    :bug: 잘못 설정된 주석 제거

--- frontend/src/pages/PrivateRoutes.tsx
+++ frontend/src/pages/PrivateRoutes.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Navigate, Outlet, useParams } from 'react-router-dom';
+import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
 
 import Loading from '@/pages/Loading/Loading';
 
@@ -8,7 +8,7 @@ import useToastStore from '@/stores/toastStore';
 import { getPairRoomExists } from '@/apis/pairRoom';
 
 const PrivateRoutes = () => {
-  // const location = useLocation();
+  const location = useLocation();
   const { accessCode } = useParams();
 
   const [isValid, setIsValid] = useState<boolean | null>(null);
@@ -16,11 +16,11 @@ const PrivateRoutes = () => {
   const { addToast } = useToastStore();
 
   const validateAccess = async () => {
-    // if (!accessCode || !location.state?.valid) {
-    //   setIsValid(false);
-    //   addToast({ status: 'ERROR', message: '유효하지 않은 접근입니다. 올바른 경로로 접근해 주세요.' });
-    //   return;
-    // }
+    if (!accessCode || !location.state?.valid) {
+      setIsValid(false);
+      addToast({ status: 'ERROR', message: '유효하지 않은 접근입니다. 올바른 경로로 접근해 주세요.' });
+      return;
+    }
 
     const { exists } = await getPairRoomExists(accessCode || '');
 
