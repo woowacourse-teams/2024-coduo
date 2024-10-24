cf93cba45b1c6de40173393795d8bc2dbf017a47
greetings1012 <ahnqwerty@gmail.com>
2024-10-24 21:42:25 +0900

    💄 스피너 스타일 개선

--- frontend/src/components/common/Spinner/Spinner.styles.ts
+++ frontend/src/components/common/Spinner/Spinner.styles.ts
@@ -23,6 +23,9 @@ export const Layout = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  width: 100%;
+  height: 100%;
 `;
 
 export const Spinner = styled.div<{ $size: SpinnerSize }>`
