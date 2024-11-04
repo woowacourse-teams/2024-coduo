072e6a45cfc1c9b8808ae4ec36b788ecb6a21d1a
lemone <lejinhy@gmail.com>
2024-11-04 21:20:06 +0900

    feat: 페어룸이 종료되거나 삭제된 상태일 때 발생하는 커스텀 예외 추가 및 핸들링

--- backend/src/main/java/site/coduo/pairroom/exception/DeletePairRoomException.java
+++
@@ -1,8 +0,0 @@
-package site.coduo.pairroom.exception;
-
-public class DeletePairRoomException extends PairRoomException {
-
-    public DeletePairRoomException(final String message) {
-        super(message);
-    }
-}
---
+++ backend/src/main/java/site/coduo/pairroom/exception/InactivePairRoomException.java
@@ -0,0 +1,8 @@
+package site.coduo.pairroom.exception;
+
+public class InactivePairRoomException extends PairRoomException {
+
+    public InactivePairRoomException(final String message) {
+        super(message);
+    }
+}
