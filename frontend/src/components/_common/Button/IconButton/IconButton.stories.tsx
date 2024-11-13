09efc9a8771acef443cab7839300ea44d015b80e
greetings1012 <ahnqwerty@gmail.com>
2024-11-13 17:07:13 +0900

    ✅ 스토리북 작성

---
+++ frontend/src/components/_common/Button/IconButton/IconButton.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, StoryFn } from '@storybook/react';
+import { FaBeer } from 'react-icons/fa'; // Example icon from react-icons
+
+import IconButton from './IconButton';
+
+interface IconButtonProps {
+  icon: React.ReactElement;
+  size?: 'sm' | 'md' | 'lg' | 'xl' | string;
+  backgroundColor?: string;
+  isActive?: boolean;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+export default {
+  title: 'component/common/IconButton',
+  component: IconButton,
+  argTypes: {
+    size: { control: { type: 'select', options: ['sm', 'md', 'lg', 'xl'] } },
+    backgroundColor: { control: 'color' },
+    isActive: { control: 'boolean' },
+  },
+} as Meta;
+
+const Template: StoryFn<IconButtonProps> = (args: IconButtonProps) => <IconButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  icon: <FaBeer />,
+  size: 'md',
+  backgroundColor: 'black',
+  isActive: true,
+};
+
+export const LargeInactive = Template.bind({});
+LargeInactive.args = {
+  icon: <FaBeer />,
+  size: 'lg',
+  backgroundColor: 'gray',
+  isActive: false,
+  disabled: true,
+};
+
+export const CustomColor = Template.bind({});
+CustomColor.args = {
+  icon: <FaBeer />,
+  size: 'md',
+  backgroundColor: '#a21321',
+  isActive: true,
+};
