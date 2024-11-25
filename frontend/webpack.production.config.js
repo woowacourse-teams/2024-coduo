1a8f67ede8ab52b02c94c9b8558abaac29644275
anttiey <hanuuny@gmail.com>
2024-11-25 16:51:47 +0900

    :ambulance: 환경 변수 적용 방법 변경

--- frontend/webpack.production.config.js
+++ frontend/webpack.production.config.js
@@ -1,27 +1,38 @@
 import { merge } from 'webpack-merge';
 import common from './webpack.common.config.js';
 import { sentryWebpackPlugin } from '@sentry/webpack-plugin';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import webpack from 'webpack';
 import pkg from './package.json' with { type: 'json' };
 
-const env = dotenv.config({ path: '.env' }).parsed;
+// const env = dotenv.config({ path: '.env' }).parsed;
 
-console.log(env);
+// console.log(env);
 
-const envKeys = env
-  ? Object.keys(env).reduce((prev, next) => {
-      prev[`process.env.${next}`] = JSON.stringify(env[next]);
-      return prev;
-    }, {})
-  : {};
+// const envKeys = env
+//   ? Object.keys(env).reduce((prev, next) => {
+//       prev[`process.env.${next}`] = JSON.stringify(env[next]);
+//       return prev;
+//     }, {})
+//   : {};
 
-console.log(envKeys);
+// console.log(envKeys);
+
+const envKeys = {
+  'process.env.REACT_APP_API_URL': JSON.stringify(process.env.REACT_APP_API_URL),
+  'process.env.REACT_APP_SENTRY_AUTH_TOKEN': JSON.stringify(process.env.REACT_APP_SENTRY_AUTH_TOKEN),
+  'process.env.REACT_APP_SENTRY_DSN': JSON.stringify(process.env.REACT_APP_SENTRY_DSN),
+  'process.env.REACT_APP_GA_TRACKING_ID': JSON.stringify(process.env.REACT_APP_GA_TRACKING_ID),
+  'process.env.REACT_APP_CLIENT_ID': JSON.stringify(process.env.REACT_APP_CLIENT_ID),
+  'process.env.REACT_APP_GITHUB_AUTH': JSON.stringify(process.env.REACT_APP_GITHUB_AUTH),
+  'process.env.REACT_APP_VERSION': JSON.stringify(pkg.version),
+};
 
 export default merge(common, {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
+    new webpack.DefinePlugin(envKeys),
     sentryWebpackPlugin({
       authToken: process.env.REACT_APP_SENTRY_AUTH_TOKEN,
       org: '2024-coduo',
@@ -34,10 +45,6 @@ export default merge(common, {
       },
       include: './dist',
       ignore: ['node_modules'],
-    }),
-    new webpack.DefinePlugin({
-      ...envKeys,
-      REACT_APP_VERSION: pkg.version,
     }),
   ],
   optimization: {
