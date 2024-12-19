b6a6150a0eaf16bcfd74cba4543705d3af1f3c13
kelly6bf <kelly6bf@gmail.com>
2024-12-19 14:12:27 +0900

    test: #983 불필요한 테스트 설정 및 로직 제거

--- backend/src/test/java/site/coduo/config/TestConfig.java
+++ backend/src/test/java/site/coduo/config/TestConfig.java
@@ -5,14 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
-//import site.coduo.fake.FakeEventStreamRegistry;
 import site.coduo.fake.FakeGithubApiClient;
 import site.coduo.fake.FakeGithubOAuthClient;
 import site.coduo.fake.FixedNonceProvider;
 import site.coduo.member.client.GithubApiClient;
 import site.coduo.member.client.GithubOAuthClient;
 import site.coduo.member.infrastructure.security.NonceProvider;
-//import site.coduo.sync.service.EventStreamsRegistry;
 
 @TestConfiguration
 public class TestConfig {
@@ -40,10 +38,4 @@ public class TestConfig {
     public ThreadPoolTaskScheduler testThreadPoolTaskScheduler() {
         return new ThreadPoolTaskScheduler();
     }
-
-//    @Bean
-//    @Primary
-//    public EventStreamsRegistry testEventStreamRegistry() {
-//        return new FakeEventStreamRegistry();
-//    }
 }
