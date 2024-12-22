c3671c753f7e2c30fa155b39bbc51a13190c118a
GimGayeon <kky1373@naver.com>
2024-12-22 21:46:21 +0900

    ✏️ CoduoDocs 하드코딩 된 부분 constant 로 분리

---
+++ frontend/src/constants/coduoDocs/sidebar.ts
@@ -0,0 +1,60 @@
+export const START_WITH_MISSION = [
+  {
+    id: 'start-with-mission',
+    subtitle: '미션과 함께 시작하기',
+  },
+  {
+    id: 'select-mission',
+    subtitle: '미션 확인하기',
+  },
+  {
+    id: 'create-branch',
+    subtitle: '브랜치 생성하기',
+  },
+  {
+    id: 'check-branch-created',
+    subtitle: '생성한 브랜치 확인하기',
+  },
+  {
+    id: 'fork-repository',
+    subtitle: 'fork 하기',
+  },
+  {
+    id: 'copy-repository-address',
+    subtitle: '레포지토리 주소 복사하기',
+  },
+  {
+    id: 'clone-in-local',
+    subtitle: '로컬에 clone 하기',
+  },
+];
+
+export const START_FREE = [
+  {
+    id: 'start-without-mission',
+    subtitle: '미션 없이 시작하기',
+  },
+  {
+    id: 'link-pair',
+    subtitle: '이름 입력하기',
+  },
+  {
+    id: 'select-role',
+    subtitle: '역할 정하기',
+  },
+  {
+    id: 'set-timer',
+    subtitle: '타이머 설정하기',
+  },
+];
+
+export const ABOUT_PAIR_PROGRAMMING = [
+  {
+    id: 'what-is-pair-programming',
+    subtitle: '페어 프로그래밍이란?',
+  },
+  {
+    id: 'what-is-pair-room',
+    subtitle: '페어룸이란?',
+  },
+];
