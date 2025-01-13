71bcf3b15cd3f2003615f884d6eecc1270daa0f2
fram1998 <koust6u@pusan.ac.kr>
2025-01-13 15:07:52 +0900

    refactor: 컨벤션 위반 패키지 구조 수정

--- backend/src/main/java/site/coduo/pairroom/controller/docs/PairRoomDocs.java
+++ backend/src/main/java/site/coduo/pairroom/controller/docs/PairRoomDocs.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import site.coduo.pairroom.controller.dto.request.ExistMemberInPairRoomResponse;
+import site.coduo.pairroom.service.dto.ExistMemberInPairRoomResponse;
 import site.coduo.pairroom.service.dto.PairRoomCreateRequest;
 import site.coduo.pairroom.service.dto.PairRoomCreateResponse;
 import site.coduo.pairroom.service.dto.PairRoomExistResponse;
