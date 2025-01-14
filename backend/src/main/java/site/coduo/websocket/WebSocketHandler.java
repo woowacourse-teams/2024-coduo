e37dd1baad13cbee0bd5e430a7f1fd82fe372d6e
lemone <lejinhy@gmail.com>
2025-01-14 15:49:21 +0900

    feat: 웹소켓 커넥션이 종료되었을 때 & 에러가 발생했을 때 타이머 remaining time을 DB에 반영

--- backend/src/main/java/site/coduo/websocket/WebSocketHandler.java
+++ backend/src/main/java/site/coduo/websocket/WebSocketHandler.java
@@ -38,12 +38,15 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTransportError(final WebSocketSession session, final Throwable exception) {
+        final String pairRoomAccessCode = parsePairRoomAccessCode(session);
+        schedulerService.flushTimerToDB(pairRoomAccessCode);
         log.error("Web Socket 전송 중 에러 발생 : {}", exception.getMessage());
     }
 
     @Override
     public void afterConnectionClosed(final WebSocketSession session, final CloseStatus status) {
         final String pairRoomAccessCode = parsePairRoomAccessCode(session);
+        schedulerService.flushTimerToDB(pairRoomAccessCode);
         pairRoomWebSocketSessionStore.removeSession(pairRoomAccessCode, session);
         log.info("연결 종료 : {}, 상태 : {}", session.getId(), status);
     }
