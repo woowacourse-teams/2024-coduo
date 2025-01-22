67e1bab2a2cf1b70c9db514654451daf6a1c79b8
greetings1012 <ahnqwerty@gmail.com>
2025-01-22 16:41:39 +0900

    ✏️ Publish 엔드포인트 프리픽스 변경

--- frontend/src/apis/websocket/todo.ts
+++ frontend/src/apis/websocket/todo.ts
@@ -4,18 +4,18 @@ import { publishMessage } from '@/apis/websocket/websocket';
 
 export const publishTodoMessage = {
   add: (client: Client | null, accessCode: string, contents: string) => {
-    publishMessage(client, `/topic/${accessCode}/todo/add`, { contents });
+    publishMessage(client, `/send/${accessCode}/todo/add`, { contents });
   },
   updateContents: (client: Client | null, accessCode: string, todoId: number, contents: string) => {
-    publishMessage(client, `/topic/${accessCode}/todo/update/${todoId}/contents`, { contents });
+    publishMessage(client, `/send/${accessCode}/todo/update/${todoId}/contents`, { contents });
   },
   updateOrder: (client: Client | null, accessCode: string, todoId: number, order: number) => {
-    publishMessage(client, `/topic/${accessCode}/todo/update/${todoId}/order`, { order });
+    publishMessage(client, `/send/${accessCode}/todo/update/${todoId}/order`, { order });
   },
   updateChecked: (client: Client | null, accessCode: string, todoId: number) => {
-    publishMessage(client, `/topic/${accessCode}/todo/update/${todoId}/checked`);
+    publishMessage(client, `/send/${accessCode}/todo/update/${todoId}/checked`);
   },
   delete: (client: Client | null, accessCode: string, todoId: number) => {
-    publishMessage(client, `/topic/${accessCode}/todo/delete/${todoId}`);
+    publishMessage(client, `/send/${accessCode}/todo/delete/${todoId}`);
   },
 };
