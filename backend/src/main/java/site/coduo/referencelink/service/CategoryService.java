96ada21d99106b8e183ff1a2288bbf70908b904e
yechop <channetattoo@gmail.com>
2025-02-12 16:33:35 +0900

    refactor: 웹소켓 dto 변수명 변경

--- backend/src/main/java/site/coduo/referencelink/service/CategoryService.java
+++ backend/src/main/java/site/coduo/referencelink/service/CategoryService.java
@@ -43,9 +43,9 @@ public class CategoryService {
     public CategoryCreateResponse createCategory(final String accessCode, final CategoryCreateRequest request) {
         final PairRoomEntity pairRoomEntity = pairRoomRepository.fetchByAccessCode(new AccessCode(accessCode));
         checkPairRoomIsActive(pairRoomEntity);
-        validateDuplicated(request.value(), pairRoomEntity);
+        validateDuplicated(request.categoryName(), pairRoomEntity);
         final CategoryEntity categoryEntity = categoryRepository.save(
-                new CategoryEntity(pairRoomEntity, new Category(request.value())));
+                new CategoryEntity(pairRoomEntity, new Category(request.categoryName())));
 
         return CategoryCreateResponse.from(categoryEntity);
     }
@@ -59,10 +59,10 @@ public class CategoryService {
     public CategoryUpdateResponse updateCategoryName(final String accessCode, final CategoryUpdateRequest request) {
         final PairRoomEntity pairRoomEntity = pairRoomRepository.fetchByAccessCode(new AccessCode(accessCode));
         checkPairRoomIsActive(pairRoomEntity);
-        validateDuplicated(request.updatedCategoryName(), pairRoomEntity);
+        validateDuplicated(request.categoryName(), pairRoomEntity);
         final CategoryEntity category = categoryRepository.fetchByPairRoomAndCategoryId(pairRoomEntity,
                 request.categoryId());
-        category.updateCategoryName(request.updatedCategoryName());
+        category.updateCategoryName(request.categoryName());
         return new CategoryUpdateResponse(category.getCategoryName());
     }
 
