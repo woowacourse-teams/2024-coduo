7324775b1071168c1002f9fdc52ed4a58c197755
greetings1012 <ahnqwerty@gmail.com>
2025-02-12 14:49:37 +0900

    🏗️ 레퍼런스 링크 및 카테고리 publish 로직 구현

---
+++ frontend/src/apis/websocket/referenceLink.ts
@@ -0,0 +1,24 @@
+import { Client } from '@stomp/stompjs';
+
+import { publishMessage } from '@/apis/websocket/websocket';
+
+export const publishReferenceLinkMessage = {
+  add: (client: Client | null, accessCode: string, url: string, categoryId: number) => {
+    publishMessage(client, `/send/${accessCode}/referenceLink/post`, { url, categoryId });
+  },
+  delete: (client: Client | null, accessCode: string, referenceLinkId: number) => {
+    publishMessage(client, `/send/${accessCode}/referenceLink/delete/${referenceLinkId}`);
+  },
+};
+
+export const publishCategoryMessage = {
+  add: (client: Client | null, accessCode: string, categoryName: string) => {
+    publishMessage(client, `/send/${accessCode}/category/post`, { categoryName });
+  },
+  update: (client: Client | null, accessCode: string, categoryId: number, categoryName: string) => {
+    publishMessage(client, `/send/${accessCode}/category/update/${categoryId}/name`, { categoryName });
+  },
+  delete: (client: Client | null, accessCode: string, categoryId: number) => {
+    publishMessage(client, `/send/${accessCode}/category/delete/${categoryId}`);
+  },
+};
