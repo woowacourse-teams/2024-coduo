f263fdafe86bd46562ae85a4dd62770c4b7a3e23
greetings1012 <ahnqwerty@gmail.com>
2025-02-17 23:17:03 +0900

    🚑️ 카테고리 필터링 로직 추가

--- frontend/src/hooks/PairRoom/useReferenceSocket.ts
+++ frontend/src/hooks/PairRoom/useReferenceSocket.ts
@@ -5,12 +5,17 @@ import useSocketStore from '@/stores/socketStore';
 import { Reference } from '@/apis/http/referenceLink';
 import { subscribeTopic } from '@/apis/websocket/websocket';
 
-const useReference = (defaultReferences: Reference[]) => {
+const useReference = (defaultReferences: Reference[], categoryName: string | null) => {
   const { client, isConnected, accessCode } = useSocketStore();
 
   const [references, setReferences] = useState<Reference[]>(defaultReferences);
 
-  const handleReferences = (references: Reference[]) => setReferences(references);
+  const handleReferences = (references: Reference[]) => {
+    if (categoryName) {
+      setReferences(references.filter((reference) => reference.categoryName === categoryName));
+    }
+    setReferences(references);
+  };
 
   useEffect(() => {
     if (client && isConnected) {
