5aff18c2e3057efde870e23b57fca4379954292f
greetings1012 <ahnqwerty@gmail.com>
2025-02-26 15:44:43 +0900

    🚑️ Stomp 재구독 로직 추가

--- frontend/src/apis/websocket/websocket.ts
+++ frontend/src/apis/websocket/websocket.ts
@@ -7,15 +7,32 @@ export const getConnection = () => {
 };
 
 export const subscribeTopic = <T>(client: Client | null, destination: string, handler: (body: T) => void) => {
-  if (!client?.connected) {
-    console.error('웹소켓 연결에 실패했습니다.');
+  if (!client) {
+    console.error('웹소켓 클라이언트가 없습니다.');
     return;
   }
 
-  client.subscribe(destination, (message: Message) => {
-    const body: T = JSON.parse(message.body);
-    handler(body);
-  });
+  const subscribe = () => {
+    if (client.connected) {
+      client.subscribe(destination, (message: Message) => {
+        const body: T = JSON.parse(message.body);
+        handler(body);
+      });
+    } else {
+      console.error('웹소켓 연결에 실패했습니다.');
+    }
+  };
+
+  client.onConnect = () => {
+    console.log('웹소켓 연결이 성공적으로 재설정되었습니다.');
+    subscribe();
+  };
+
+  client.onDisconnect = () => {
+    console.warn('웹소켓 연결이 해제되었습니다.');
+  };
+
+  subscribe();
 };
 
 export const publishMessage = (client: Client | null, destination: string, contents?: object) => {
